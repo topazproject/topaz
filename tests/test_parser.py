c286f14babe52c848739ca43faa84c8a01d9d25a
Alex Gaynor <alex.gaynor@gmail.com>
2012-04-07 21:55:06 -0400

    Multiple statements (parse).

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -7,3 +7,13 @@ class TestParser(object):
 
     def test_binary_expression(self, space):
         assert space.parse("1+1") == Block([Statement(BinOp("+", ConstantInt(1), ConstantInt(1)))])
+
+    def test_multiple_statements(self, space):
+        r = space.parse("""
+        1
+        2
+        """)
+        assert r == Block([
+            Statement(ConstantInt(1)),
+            Statement(ConstantInt(2)),
+        ])
