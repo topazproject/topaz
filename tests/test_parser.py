f47ecdc3b51b8901288db92fec5e4bbca1cff228
Alex Gaynor <alex.gaynor@gmail.com>
2012-04-07 21:58:14 -0400

    Multiplication support (parser).

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -8,6 +8,10 @@ class TestParser(object):
     def test_binary_expression(self, space):
         assert space.parse("1+1") == Block([Statement(BinOp("+", ConstantInt(1), ConstantInt(1)))])
 
+    def test_multi_term_expr(self, space):
+        assert space.parse("1 + 2 * 3") == Block([Statement(BinOp("+", ConstantInt(1), BinOp("*", ConstantInt(2), ConstantInt(3))))])
+        assert space.parse("1 * 2 + 3") == Block([Statement(BinOp("+", BinOp("*", ConstantInt(1), ConstantInt(2)), ConstantInt(3)))])
+
     def test_multiple_statements(self, space):
         r = space.parse("""
         1
