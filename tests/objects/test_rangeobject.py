f183c3561d189df3dc274798f9dcba52f3b101cb
Alex Gaynor <alex.gaynor@gmail.com>
2012-05-05 10:04:25 -0500

    make test faster.

--- tests/objects/test_rangeobject.py
+++ tests/objects/test_rangeobject.py
@@ -4,11 +4,11 @@ import py
 class TestRangeObject(object):
     def test_map(self, space):
         w_res = space.execute("""
-        return (1..5).map do |x|
+        return (1..3).map do |x|
             x * 5
         end
         """)
-        assert [space.int_w(w_x) for w_x in w_res.items_w] == [5, 10, 15, 20]
+        assert [space.int_w(w_x) for w_x in w_res.items_w] == [5, 10]
 
     def test_starting_point_always_returned(self, space):
         w_res = space.execute("""
