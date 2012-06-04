b3247b003b410b711860984476800ef8d29d2b35
Curt Micol <asenchi@asenchi.com>
2012-06-04 10:37:43 -0400

    Cleanup comparable

--- rupypy/modules/comparable.py
+++ rupypy/modules/comparable.py
@@ -1,5 +1,6 @@
 from rupypy.module import Module, ModuleDef
 
+
 class Comparable(Module):
     moduledef = ModuleDef("Comparable")
 
@@ -21,24 +22,14 @@ class Comparable(Module):
     end
     """)
 
-    # moduledef.app_method("""
-    # def <= other
-    #     if self < other
-    #         return true
-    #     elsif self == other
-    #         return true
-    #     end
-    #     return false
-    # end
-    # """)
+    moduledef.app_method("""
+    def <= other
+        return ((self <=> other) <=> 0) < 0
+    end
+    """)
 
-    # moduledef.app_method("""
-    # def >= other
-    #     if self > other
-    #         return true
-    #     elsif self == other
-    #         return true
-    #     end
-    #     return false
-    # end
-    # """)
+    moduledef.app_method("""
+    def >= other
+        return ((self <=> other) <=> 0) > 0
+    end
+    """)
--- tests/modules/test_comparable.py
+++ tests/modules/test_comparable.py
@@ -6,11 +6,3 @@ class TestComparable(object):
     def test_greater(self, ec):
         w_res = ec.space.execute(ec, "return 1 > 2")
         assert w_res is ec.space.w_false
-
-    # def test_less_equal(self, ec):
-    #     w_res = ec.space.execute(ec, "return 2 <= 2")
-    #     assert w_res is ec.space.w_true
-
-    # def test_greater_equal(self, ec):
-    #     w_res = ec.space.execute(ec, "return 2 >= 2")
-    #     assert w_res is ec.space.w_true
