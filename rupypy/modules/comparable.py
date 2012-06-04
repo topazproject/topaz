94ab12a38492cf5c58f9ce75598ba50ac8cc0de3
Curt Micol <asenchi@asenchi.com>
2012-06-04 11:57:47 -0400

    Remove these since they aren't working. Will come in another PR

--- rupypy/modules/comparable.py
+++ rupypy/modules/comparable.py
@@ -3,33 +3,3 @@ from rupypy.module import Module, ModuleDef
 
 class Comparable(Module):
     moduledef = ModuleDef("Comparable")
-
-    moduledef.app_method("""
-    def < other
-        if self < other
-            return true
-        end
-        return false
-    end
-    """)
-
-    moduledef.app_method("""
-    def > other
-        if self > other
-            return true
-        end
-        return false
-    end
-    """)
-
-    moduledef.app_method("""
-    def <= other
-        return ((self <=> other) <=> 0) < 0
-    end
-    """)
-
-    moduledef.app_method("""
-    def >= other
-        return ((self <=> other) <=> 0) > 0
-    end
-    """)
--- tests/modules/test_comparable.py
+++ tests/modules/test_comparable.py
@@ -1,8 +1,2 @@
 class TestComparable(object):
-    def test_less(self, ec):
-        w_res = ec.space.execute(ec, "return 'a' < 'b'")
-        assert w_res is ec.space.w_true
-
-    def test_greater(self, ec):
-        w_res = ec.space.execute(ec, "return 'a' > 'b'")
-        assert w_res is ec.space.w_false
+    pass
