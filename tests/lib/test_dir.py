ba91a14f655b97b219343e92649e26478782bd21
Alex Gaynor <alex.gaynor@gmail.com>
2012-06-15 11:42:18 -0500

    Added Dir#pwd

--- tests/lib/test_dir.py
+++ tests/lib/test_dir.py
@@ -1,3 +1,10 @@
+import os
+
+
 class TestDir(object):
     def test_name(self, ec):
         ec.space.execute(ec, "Dir")
+
+    def test_pwd(self, ec):
+        w_res = ec.space.execute(ec, "return Dir.pwd")
+        assert ec.space.str_w(w_res) == os.getcwd()
