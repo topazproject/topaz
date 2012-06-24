fd8c4a4b96f4eb2a0097d6cfd290098c945e5670
Alex Gaynor <alex.gaynor@gmail.com>
2012-06-24 08:02:46 -0700

    explanatory comment

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,11 @@ def pytest_funcarg__space(request):
     # Inside the function so various intitialization stuff isn't seen until
     # coverage is setup.
     from rupypy.objspace import ObjectSpace
+
+    # Building a space is exceptionally expensive, so we create one once, and
+    # then just deepcopy it.  Note that deepcopying is still fairly expensive
+    # (at the time of writing about 1/3 of total test time), but significantly
+    # less so than building a new space.
     space = request.cached_setup(
         setup=ObjectSpace,
         scope="session",
