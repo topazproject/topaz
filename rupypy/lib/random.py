32a5e19ba33fada5b296b8acef123d67f103e6fa
David Schneider <david.schneider@picle.org>
2012-06-27 19:44:54 +0200

    allow subclassing Random objects

--- rupypy/lib/random.py
+++ rupypy/lib/random.py
@@ -7,13 +7,13 @@ from rupypy.objects.objectobject import W_Object
 class W_Random(W_Object):
     classdef = ClassDef("Random", W_Object.classdef)
 
-    def __init__(self, space):
-        W_Object.__init__(self, space)
+    def __init__(self, space, klass=None):
+        W_Object.__init__(self, space, klass)
         self.random = Random()
 
     @classdef.singleton_method("allocate")
     def method_allocate(self, space):
-        return W_Random(space)
+        return W_Random(space, self)
 
     @classdef.method("rand")
     def method_rand(self, space):
--- tests/lib/test_random.py
+++ tests/lib/test_random.py
@@ -9,3 +9,17 @@ class TestRandom(object):
     def test_rand(self, space):
         w_res = space.execute("return Random.new.rand")
         assert 0 < space.float_w(w_res) < 1
+
+    def test_subclass(self, space):
+        w_res = space.execute("""
+        class SubRandom < Random
+            def better_rand
+                4 # http://xkcd.com/221/
+            end
+        end
+        c = SubRandom.new
+        return [c.rand, c.better_rand]
+        """)
+        res = space.listview(w_res)
+        assert 0 < space.float_w(res[0]) < 1
+        assert space.int_w(res[1]) == 4
