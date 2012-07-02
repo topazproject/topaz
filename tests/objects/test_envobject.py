d5ea7dd604e4fd5b8b3f80202a2e2d7a12993dc3
Tim Felgentreff <timfelgentreff@gmail.com>
2012-07-02 16:45:46 +0200

    test r/w access to the environment dictionary

--- tests/objects/test_envobject.py
+++ tests/objects/test_envobject.py
@@ -1,3 +1,5 @@
+import os
+
 from rupypy.objects.objectobject import W_Object
 
 from ..base import BaseRuPyPyTest
@@ -7,3 +9,18 @@ class TestEnvObject(BaseRuPyPyTest):
     def test_class(self, space):
         w_res = space.execute("return ENV.class")
         assert w_res == self.find_const(space, "Object")
+
+    def test_access_env(self, space, monkeypatch):
+        env = {"HOME": "/home/test"}
+        monkeypatch.setattr(os, 'environ', env)
+
+        w_res = space.execute("return ENV['HOME']")
+        assert space.str_w(w_res) == "/home/test"
+        w_res = space.execute("return ENV['HEIM']")
+        assert w_res == space.w_nil
+        w_res = space.execute("""
+        ENV['HOME'] = '/home/newhome'
+        return ENV['HOME']
+        """)
+        assert space.str_w(w_res) == "/home/newhome"
+        assert env["HOME"] == "/home/newhome"
