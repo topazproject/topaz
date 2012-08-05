66d055fbaa7e08271c5cf5e46ae76bb011be7811
Alex Gaynor <alex.gaynor@gmail.com>
2012-08-05 14:59:31 -0700

    On travis compile the docs to verify they're valid reST.

--- fabfile/travis.py
+++ fabfile/travis.py
@@ -27,10 +27,14 @@ def run_own_tests(env):
 
 
 def run_translate_tests(env):
-    local("PYTHONPATH=%(pypy_path)s:$PYTHONPATH %(pypy_path)s/pypy/translator/goal/translate.py -Ojit targetrupypy.py" % env)
+    local("PYTHONPATH=%(pypy_path)s:$PYTHONPATH %(pypy_path)s/pypy/translator/goal/translate.py --batch -Ojit targetrupypy.py" % env)
+
+def run_docs_tests(env):
+    with lcd("docs"):
+        local("sphinx-build -W -b html")
 
 TEST_TYPES = {
     "own": run_own_tests,
     "translate": run_translate_tests,
+    "docs": run_docs_tests,
 }
-
