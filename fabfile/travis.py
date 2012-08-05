720e8e2fbeebb919db1f9ea3ccc87dee10bc7daa
Alex Gaynor <alex.gaynor@gmail.com>
2012-08-05 15:30:05 -0700

    only install the needed deps

--- fabfile/travis.py
+++ fabfile/travis.py
@@ -4,22 +4,45 @@ import os
 from fabric.api import task, local, lcd
 
 
+class Test(object):
+    def __init__(self, func, deps=[], needs_pypy=True):
+        self.func = func
+        self.deps = deps
+        self.needs_pypy = needs_pypy
+
+    def install_deps(self):
+        local("pip install {}".format(" ".join(self.deps)))
+
+    def download_pypy(self):
+        local("wget https://bitbucket.org/pypy/pypy/get/default.tar.bz2 -O `pwd`/../pypy.tar.bz2")
+        local("bunzip2 `pwd`/../pypy.tar.bz2")
+        local("tar -xf `pwd`/../pypy.tar -C `pwd`/../")
+        [path_name] = glob.glob("../pypy-pypy*")
+        path_name = os.path.abspath(path_name)
+        with open("pypy_marker", "w") as f:
+            f.write(path_name)
+
+    def run_tests(self):
+        env = {}
+        if self.needs_pypy:
+            with open("pypy_marker") as f:
+                env["pypy_path"] = f.read()
+        self.func(env)
+
+
 @task
-def download_pypy():
-    local("wget https://bitbucket.org/pypy/pypy/get/default.tar.bz2 -O `pwd`/../pypy.tar.bz2")
-    local("bunzip2 `pwd`/../pypy.tar.bz2")
-    local("tar -xf `pwd`/../pypy.tar -C `pwd`/../")
-    [path_name] = glob.glob("../pypy-pypy*")
-    path_name = os.path.abspath(path_name)
-    with open("pypy_marker", "w") as f:
-        f.write(path_name)
+def install_requirements():
+    t = TEST_TYPES[os.environ["TEST_TYPE"]]
+    if t.deps:
+        t.install_deps()
+    if t.needs_pypy:
+        t.download_pypy()
 
 
 @task
 def run_tests():
-    with open("pypy_marker") as f:
-        path_name = f.read()
-    TEST_TYPES[os.environ["TEST_TYPE"]]({"pypy_path": path_name})
+    t = TEST_TYPES[os.environ["TEST_TYPE"]]
+    t.run_tests()
 
 
 def run_own_tests(env):
@@ -31,11 +54,10 @@ def run_translate_tests(env):
 
 
 def run_docs_tests(env):
-    with lcd("docs"):
-        local("sphinx-build -W -b html")
+    local("sphinx-build -W -b html docs/ docs/_build/")
 
 TEST_TYPES = {
-    "own": run_own_tests,
-    "translate": run_translate_tests,
-    "docs": run_docs_tests,
+    "own": Test(run_own_tests, deps=["pytest"]),
+    "translate": Test(run_translate_tests),
+    "docs": Test(run_docs_tests, deps=["sphinx"], needs_pypy=False),
 }
