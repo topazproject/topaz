8a784f35544137f8f4da3c89738d0ec23699039e
Alex Gaynor <alex.gaynor@rd.io>
2012-09-28 20:15:48 -0700

    obnoxious workaround for pypy failing

--- rupypy/objects/stringobject.py
+++ rupypy/objects/stringobject.py
@@ -78,7 +78,9 @@ class MutableStringStrategy(StringStrategy):
         storage = self.unerase(storage)
         changed = False
         for i, c in enumerate(storage):
-            new_c = c.lower()
+            # TODO: obscure hack because lower() returns a string, rather than
+            # a char, this should be fixed upstream.
+            new_c = c.lower()[0]
             changed |= (c != new_c)
             storage[i] = new_c
         return changed
