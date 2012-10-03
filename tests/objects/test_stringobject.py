979803a3609ff206f0ee4d51614a874cb2544ec9
Tim Felgentreff <timfelgentreff@gmail.com>
2012-10-03 20:43:58 +0200

    use available regexp engine to finish string#split

--- tests/objects/test_stringobject.py
+++ tests/objects/test_stringobject.py
@@ -141,6 +141,20 @@ class TestStringObject(BaseRuPyPyTest):
         assert self.unwrap(space, w_res) == ["a", "b-c"]
         w_res = space.execute("return 'a b c'.split(' ', -1)")
         assert self.unwrap(space, w_res) == ["a", "b", "c"]
+        with self.raises(space, "TypeError", "wrong argument type Fixnum (expected Regexp)"):
+            space.execute("'a b c'.split(12)")
+
+    def test_split_regexp(self, space):
+        w_res = space.execute("return \"now's  the time\".split(/ /)")
+        assert self.unwrap(space, w_res) == ["now's", "", "the", "time"]
+        w_res = space.execute('return "1, 2.34,56, 7".split(%r{,\s*})')
+        assert self.unwrap(space, w_res) == ["1", "2.34", "56", "7"]
+        w_res = space.execute('return "hello".split(//)')
+        assert self.unwrap(space, w_res) == ["h", "e", "l", "l", "o"]
+        # w_res = space.execute('return "hello".split(//, 3)')
+        # assert self.unwrap(space, w_res) == ["h", "e", "llo"]
+        # w_res = space.execute('return "hi mom".split(%r{\s*})')
+        # assert self.unwrap(space, w_res) == ["h", "i", "m", "o", "m"]
 
     def test_dup(self, space):
         w_res = space.execute("""
