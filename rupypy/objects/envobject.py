d57e32463c44afde576f53aa383184b7e162d03a
Tim Felgentreff <timfelgentreff@gmail.com>
2012-10-04 09:39:26 +0200

    add ENV[]= and use proper type conversions in ENV

--- rupypy/objects/envobject.py
+++ rupypy/objects/envobject.py
@@ -11,10 +11,22 @@ class W_EnvObject(W_Object):
     def setup_class(cls, space, w_cls):
         space.set_const(space.w_object, "ENV", cls(space))
 
-    @classdef.method("[]", key="str")
-    def method_subscript(self, space, key):
+    @classdef.method("[]")
+    def method_subscript(self, space, w_key):
+        key = space.str_w(space.convert_type(w_key, space.w_string, "to_str"))
         try:
             val = os.environ[key]
         except KeyError:
             return space.w_nil
         return space.newstr_fromstr(val)
+
+    @classdef.method("[]=")
+    def method_subscript_assign(self, space, w_key, w_value):
+        key = space.str_w(space.convert_type(w_key, space.w_string, "to_str"))
+        value = space.str_w(space.convert_type(w_value, space.w_string, "to_str"))
+        os.environ[key] = value
+        return w_value
+
+    @classdef.method("class")
+    def method_class(self, space):
+        return space.w_object
--- tests/objects/test_envobject.py
+++ tests/objects/test_envobject.py
@@ -1,3 +1,5 @@
+import os
+
 from ..base import BaseRuPyPyTest
 
 
@@ -11,3 +13,26 @@ class TestEnvObject(BaseRuPyPyTest):
         monkeypatch.delenv("ZZZZ", raising=False)
         w_res = space.execute("return ENV['ZZZZ']")
         assert self.unwrap(space, w_res) is None
+
+    def test_set(self, space, monkeypatch):
+        w_res = space.execute("""
+        ENV['ZZZZ'] = '/home/newhome'
+        return ENV['ZZZZ']
+        """)
+        assert space.str_w(w_res) == "/home/newhome"
+        assert os.environ.get("ZZZZ") == "/home/newhome"
+        monkeypatch.delenv("ZZZZ", raising=False)
+
+    def test_set_get_stringish(self, space, monkeypatch):
+        w_res = space.execute("""
+        class A; def to_str; "ZZZZ"; end; end
+        ENV[A.new] = A.new
+        return ENV[A.new]
+        """)
+        assert space.str_w(w_res) == "ZZZZ"
+        assert os.environ.get("ZZZZ") == "ZZZZ"
+        monkeypatch.delenv("ZZZZ", raising=False)
+
+    def test_class(self, space):
+        w_res = space.execute("return ENV.class")
+        assert w_res == self.find_const(space, "Object")
