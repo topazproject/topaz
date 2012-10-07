b8b508e879389ee7f8f4903c18833f0e4290a045
Tim Felgentreff <timfelgentreff@gmail.com>
2012-10-07 16:49:51 +0200

    write in ruby

--- rupypy/objects/fileobject.py
+++ rupypy/objects/fileobject.py
@@ -98,11 +98,12 @@ class W_IOObject(W_Object):
         else:
             return w_read_str
 
-    @classdef.method("<<")
-    def method_push(self, space, w_str):
-        string = space.str_w(space.send(w_str, space.newsymbol("to_s")))
-        os.write(self.fd, string)
+    classdef.app_method("""
+    def << s
+        write(s)
         return self
+    end
+    """)
 
     @classdef.method("write")
     def method_write(self, space, w_str):
