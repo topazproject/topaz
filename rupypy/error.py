29efd8c0d3888054fb869f76fca62b0307db8408
Alex Gaynor <alex.gaynor@rd.io>
2012-10-16 07:23:48 -0700

    name nits

--- rupypy/error.py
+++ rupypy/error.py
@@ -10,11 +10,11 @@ class RubyError(Exception):
 
 
 def format_traceback(space, exc):
-    w_ary = space.send(exc, space.newsymbol("backtrace"))
-    assert space.getclass(w_ary) is space.w_array
-    ary = space.listview(w_ary)
-    yield "%s: %s (%s)\n" % (space.str_w(ary[0]), exc.msg, space.getclass(exc).name)
-    for w_line in ary[1:]:
+    w_bt = space.send(exc, space.newsymbol("backtrace"))
+    assert space.getclass(w_bt) is space.w_array
+    bt_w = space.listview(w_bt)
+    yield "%s: %s (%s)\n" % (space.str_w(bt_w[0]), exc.msg, space.getclass(exc).name)
+    for w_line in bt_w[1:]:
         yield "\tfrom %s\n" % space.str_w(w_line)
 
 
