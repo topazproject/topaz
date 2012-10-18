d19113822d3a025749454de9d41006d2de057704
Greg Price <price@mit.edu>
2012-10-18 02:30:04 -0400

    Provide for global variables as getters and setters

--- tests/test_celldict.py
+++ tests/test_celldict.py
@@ -1,7 +1,9 @@
-from rupypy.celldict import CellDict, Cell
+from rupypy.celldict import CellDict, Cell, GlobalsDict
+
+from .base import BaseRuPyPyTest
 
 
-class TestCellDict(object):
+class TestCellDict(BaseRuPyPyTest):
     def test_single_set(self):
         c = CellDict()
         v = c.version
@@ -20,3 +22,16 @@ class TestCellDict(object):
         c.set("a", 4)
         assert isinstance(c._get_cell("a", c.version), Cell)
         assert c.version is v
+
+    def test_globals(self, space):
+        space.stuff = 4
+        g = GlobalsDict(space)
+        g.def_virtual('x', lambda s: s.stuff)
+        assert g.get('x') == 4
+        with self.raises(space, "NameError"):
+            g.set('x', 5)
+
+        g.def_virtual('y', lambda s: s.stuff, lambda s, v: setattr(s, 'stuff', v))
+        assert g.get('y') == 4
+        g.set('y', 5)
+        assert g.get('y') == 5
