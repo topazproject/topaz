cb4630c933a70cdeae19776f6cfe07046a6626b1
Alex Gaynor <alex.gaynor@rd.io>
2012-10-30 08:22:54 -0700

    bignum.coerce

--- tests/objects/test_bignumobject.py
+++ tests/objects/test_bignumobject.py
@@ -1,7 +1,9 @@
 from pypy.rlib.rbigint import rbigint
 
+from ..base import BaseRuPyPyTest
 
-class TestBignumObject(object):
+
+class TestBignumObject(BaseRuPyPyTest):
     def test_plus(self, space):
         w_res = space.execute("return 18446744073709551628 + 9")
         assert space.bigint_w(w_res) == rbigint.fromlong(18446744073709551637)
@@ -33,3 +35,13 @@ class TestBignumObject(object):
         assert w_res is space.w_true
         w_res = space.execute("return 18446744073709551628.hash == 18446744073709551658.hash")
         assert w_res is space.w_false
+
+    def test_coerce(self, space):
+        w_res = space.execute("return 18446744073709551628.coerce 12")
+        assert self.unwrap(space, w_res) == [rbigint.fromint(12), rbigint.fromlong(18446744073709551628)]
+
+        w_res = space.execute("return 18446744073709551628.coerce 18446744073709551628")
+        assert self.unwrap(space, w_res) == [rbigint.fromlong(18446744073709551628), rbigint.fromlong(18446744073709551628)]
+
+        with self.raises(space, "TypeError", "can't coerce String to Bignum"):
+            space.execute("18446744073709551628.coerce 'hello'")
