90bdf0054a11ba1c33c75113ef1223051528e32e
Alex Gaynor <alex.gaynor@rd.io>
2012-10-31 21:58:09 -0700

    can't break from an exited frame

--- rupypy/interpreter.py
+++ rupypy/interpreter.py
@@ -29,38 +29,44 @@ class Interpreter(object):
         get_printable_location=get_printable_location,
     )
 
+    def __init__(self):
+        self.finished = False
+
     def get_block_bytecode(self, block):
         return block.bytecode if block is not None else None
 
     def interpret(self, space, frame, bytecode):
         pc = 0
         try:
-            while True:
-                self.jitdriver.jit_merge_point(
-                    self=self, bytecode=bytecode, frame=frame, pc=pc,
-                    block_bytecode=self.get_block_bytecode(frame.block),
-                )
-                frame.last_instr = pc
-                # Why do we wrap the PC in an object? The JIT has store
-                # sinking, but when it encounters a guard it usually performs
-                # all pending stores, *execpt* if the value is a virtual, then
-                # it doesn't, so we make this store be of a virtual, in order
-                # to ensure the JIT sinks the store.
-                space.getexecutioncontext().last_instr_ref = IntegerWrapper(pc)
-                try:
-                    pc = self.handle_bytecode(space, pc, frame, bytecode)
-                except RubyError as e:
-                    pc = self.handle_ruby_error(space, pc, frame, bytecode, e)
-                except RaiseReturn as e:
-                    pc = self.handle_raise_return(space, pc, frame, bytecode, e)
-                except RaiseBreak as e:
-                    pc = self.handle_raise_break(space, pc, frame, bytecode, e)
-        except RaiseReturn as e:
-            if e.parent_interp is self:
+            try:
+                while True:
+                    self.jitdriver.jit_merge_point(
+                        self=self, bytecode=bytecode, frame=frame, pc=pc,
+                        block_bytecode=self.get_block_bytecode(frame.block),
+                    )
+                    frame.last_instr = pc
+                    # Why do we wrap the PC in an object? The JIT has store
+                    # sinking, but when it encounters a guard it usually performs
+                    # all pending stores, *execpt* if the value is a virtual, then
+                    # it doesn't, so we make this store be of a virtual, in order
+                    # to ensure the JIT sinks the store.
+                    space.getexecutioncontext().last_instr_ref = IntegerWrapper(pc)
+                    try:
+                        pc = self.handle_bytecode(space, pc, frame, bytecode)
+                    except RubyError as e:
+                        pc = self.handle_ruby_error(space, pc, frame, bytecode, e)
+                    except RaiseReturn as e:
+                        pc = self.handle_raise_return(space, pc, frame, bytecode, e)
+                    except RaiseBreak as e:
+                        pc = self.handle_raise_break(space, pc, frame, bytecode, e)
+            except RaiseReturn as e:
+                if e.parent_interp is self:
+                    return e.w_value
+                raise
+            except Return as e:
                 return e.w_value
-            raise
-        except Return as e:
-            return e.w_value
+        finally:
+            self.finished = True
 
     def handle_bytecode(self, space, pc, frame, bytecode):
         instr = ord(bytecode.code[pc])
@@ -599,6 +605,8 @@ class Interpreter(object):
         return frame.unrollstack_and_jump(space, BreakLoop(w_obj))
 
     def RAISE_BREAK(self, space, bytecode, frame, pc):
+        if frame.parent_interp.finished:
+            raise space.error(space.w_LocalJumpError, "break from proc-closure")
         w_value = frame.pop()
         block = frame.unrollstack(RaiseBreakValue.kind)
         if block is None:
