cc9ee02010d3e50fccf221873fe8e39999abea88
Alex Gaynor <alex.gaynor@rd.io>
2012-11-19 10:31:47 -0600

    some general house cleaning. no regexp support really yet

--- rupypy/objects/regexpobject.py
+++ rupypy/objects/regexpobject.py
@@ -9,6 +9,33 @@ class W_RegexpObject(W_Object):
         W_Object.__init__(self, space)
         self.regexp = regexp
 
+    def _check_initialized(self, space):
+        if self.regexp is None:
+            raise space.error(space.w_TypeError, "uninitialized Regexp")
+
+    @classdef.singleton_method("allocate")
+    def method_allocate(self, space, args_w):
+        return W_RegexpObject(space, None)
+
+    @classdef.singleton_method("compile")
+    def method_compile(self, space, args_w):
+        return space.send(self, space.newsymbol("new"), args_w)
+
+    @classdef.method("initialize", source="str")
+    def method_initialize(self, space, source):
+        self.regexp = source
+
+    @classdef.method("==")
+    def method_equal(self, space, w_other):
+        if self is w_other:
+            return space.w_true
+        if not isinstance(w_other, W_RegexpObject):
+            return space.w_false
+        self._check_initialized(space)
+        w_other._check_initialized(space)
+        return space.newbool(self.regexp == w_other.regexp)
+
     @classdef.method("source")
     def method_source(self, space):
+        self._check_initialized(space)
         return space.newstr_fromstr(self.regexp)
