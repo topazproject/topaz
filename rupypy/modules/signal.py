16baf89e9249ed9bfbeba10ec7677a438ec204f3
Alex Gaynor <alex.gaynor@rd.io>
2012-12-15 01:40:13 -0800

    Added a stub Signal#trap, it makes me sad but it lets rubyspec run.

--- rupypy/modules/signal.py
+++ rupypy/modules/signal.py
@@ -3,3 +3,7 @@ from rupypy.module import Module, ModuleDef
 
 class Signal(Module):
     moduledef = ModuleDef("Signal")
+
+    @moduledef.function("trap")
+    def method_trap(self, args_w):
+        pass
