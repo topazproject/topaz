0c30b6e2ffd464f175d4619121e896704814677e
Tim Felgentreff <timfelgentreff@gmail.com>
2012-12-16 16:06:02 +0100

    add Math.sqrt

--- tests/modules/test_math.py
+++ tests/modules/test_math.py
@@ -11,3 +11,7 @@ class TestMath(BaseRuPyPyTest):
     def test_exp(self, space):
         w_res = space.execute("return [Math.exp(0.0), Math.exp(1)]")
         assert self.unwrap(space, w_res) == [1, math.exp(1)]
+
+    def test_sqrt(self, space):
+        w_res = space.execute("return [Math.sqrt(4), Math.sqrt(28)]")
+        assert self.unwrap(space, w_res) == [2, math.sqrt(28)]
