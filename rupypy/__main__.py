f67884c9ebac137f5e8c50ba0541ee5cf4405e43
Alex Gaynor <alex.gaynor@rd.io>
2012-12-30 16:29:25 -0600

    update __main__

--- rupypy/__main__.py
+++ rupypy/__main__.py
@@ -7,4 +7,8 @@ from pypy.config.translationoption import get_combined_translation_config
 
 from rupypy.main import create_entry_point, get_topaz_config_options
 
+
+entry_point = create_entry_point(get_combined_translation_config(
+    overrides=get_topaz_config_options(),
+))
 sys.exit(entry_point(sys.argv))
