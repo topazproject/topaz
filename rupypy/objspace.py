053c7a508e5efc7310b3555f2ebe39aa9c365419
Alex Gaynor <alex.gaynor@rd.io>
2013-01-01 08:26:26 -0800

    tiny indentation nit

--- rupypy/objspace.py
+++ rupypy/objspace.py
@@ -588,6 +588,6 @@ class ObjectSpace(object):
 
     def any_to_s(self, w_obj):
         return "#<%s:0x%x>" % (
-                self.str_w(self.send(self.getclass(w_obj), self.newsymbol("name"))),
-                self.int_w(self.send(w_obj, self.newsymbol("__id__")))
+            self.str_w(self.send(self.getclass(w_obj), self.newsymbol("name"))),
+            self.int_w(self.send(w_obj, self.newsymbol("__id__")))
         )
