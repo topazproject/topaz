fa6d83de43638741fb4ea2d9d3b1e34ea59d5a6c
Alex Gaynor <alex.gaynor@rd.io>
2013-01-13 17:53:53 -0800

    sufficiently advanced parser to pass one test

--- tests/jit/base.py
+++ tests/jit/base.py
@@ -20,7 +20,27 @@ class BaseJITTest(object):
         storage = LoopStorage()
         traces = [SimpleParser.parse_from_input(t) for t in data]
         traces = storage.reconnect_loops(traces)
-        return traces
+        return [Trace(t) for t in traces]
 
     def matches(self, trace, expected):
-        pass
+        expected_lines = [
+            line.strip()
+            for line in expected.splitlines()
+            if line and not line.isspace()
+        ]
+        return map(str, trace) == expected_lines
+
+
+class Trace(object):
+    def __init__(self, trace):
+        self.trace = trace
+
+    @property
+    def loop(self):
+        label_seen = False
+        for idx, op in enumerate(self.trace.operations):
+            if op.name == "label":
+                if label_seen:
+                    return self.trace.operations[idx:]
+                label_seen = True
+        raise ValueError("Loop body couldn't be found")
