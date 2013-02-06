f2e67788b58732728aa2ed36c4bff861ce34ee3b
Magnus Holm <judofyr@gmail.com>
2013-02-06 22:26:57 +0100

    Add core/array/concat_spec.rb to spec list

--- fabfile/travis.py
+++ fabfile/travis.py
@@ -109,6 +109,7 @@ def run_specs(binary, prefix=""):
         "core/array/allocate_spec.rb",
         "core/array/array_spec.rb",
         "core/array/at_spec.rb",
+        "core/array/concat_spec.rb",
         "core/array/empty_spec.rb",
         "core/array/frozen_spec.rb",
         "core/array/length_spec.rb",
