b53c4f42cb2fff4e2501d14a4d45e6d1a1d2d773
Bill Mill <bill.mill@gmail.com>
2013-02-12 11:47:21 -0500

    proper comment formatting on symbol#next

--- topaz/objects/symbolobject.py
+++ topaz/objects/symbolobject.py
@@ -60,7 +60,7 @@ class W_SymbolObject(W_Object):
         self.to_s.succ.to_sym
     end
 
-    #`alias next succ` doesn't work due to this code being loaded
+    # `alias next succ` doesn't work due to this code being loaded
     # before W_SybolObject is added to the space
     def next
         succ
