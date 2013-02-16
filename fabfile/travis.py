003ddc80a4f2aaf6da068ce7c8d79e2d1ea82a36
Tim Felgentreff <timfelgentreff@gmail.com>
2013-02-16 11:20:26 +0100

    forgot to re-add this

--- fabfile/travis.py
+++ fabfile/travis.py
@@ -121,7 +121,7 @@ def run_translate_tests(env):
 
 
 def run_specs(binary, prefix=""):
-    local("{prefix}../mspec/bin/mspec -t {binary} --format=dotted --config=topaz.mspec".format(
+    local("{prefix}../mspec/bin/mspec -G fails -t {binary} --format=dotted --config=topaz.mspec".format(
         prefix=prefix,
         binary=binary
     ))
