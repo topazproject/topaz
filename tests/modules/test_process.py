e014bc238c2dcb3c699b0efb68ed88ce22090602
Jan Stępień <jstepien@users.sourceforge.net>
2013-02-17 09:16:47 +0100

    Remove Process' tests which are covered by RubySpec
    Tests of Process.waitpid and Process.waitpid2 are staying where they are as
    they're not yet covered by the spec. However, these methods are necessary
    to execute other specs, e.g Process.fork and Process.exit!.

--- tests/modules/test_process.py
+++ tests/modules/test_process.py
@@ -51,18 +51,6 @@ class TestProcess(BaseTopazTest):
         """)
         assert space.int_w(w_res) == 200
 
-    def test_wait(self, space):
-        for code in [0, 1, 173]:
-            pid = os.fork()
-            if pid == 0:
-                os._exit(code)
-            else:
-                w_res = space.execute("return Process.wait")
-                assert space.int_w(w_res) == pid
-                w_res = space.execute("return $?")
-                status = space.send(w_res, space.newsymbol("to_i"), [])
-                assert space.int_w(status) == code
-
     def test_waitpid(self, space):
         for code in [0, 1, 173]:
             pid = os.fork()
@@ -86,12 +74,3 @@ class TestProcess(BaseTopazTest):
                 assert space.int_w(returned_pid) == pid
                 code_to_i = space.send(returned_code, space.newsymbol("to_i"), [])
                 assert space.int_w(code_to_i) == code
-
-    def test_exit_bang(self, space):
-        for code in [0, 1, 173]:
-            pid = os.fork()
-            if pid == 0:
-                space.execute("Process.exit! %i" % code)
-            else:
-                _, returned_code = os.waitpid(pid, 0)
-                assert os.WEXITSTATUS(returned_code) == code
