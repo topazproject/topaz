f4d9402c6e043387ff654b872cc1714b951f7bb2
Jan Stępień <jstepien@users.sourceforge.net>
2013-02-20 15:25:53 +0100

    Remove Process.fork tests, untag core/process/fork_spec

--- tests/modules/test_process.py
+++ tests/modules/test_process.py
@@ -32,25 +32,6 @@ class TestProcess(BaseTopazTest):
         """)
         assert self.unwrap(space, w_res) == [False, 1]
 
-    def test_fork(self, space, monkeypatch, capfd):
-        monkeypatch.setattr(os, "fork", lambda: 0)
-        with self.raises(space, "SystemExit"):
-            space.execute("""
-            Process.fork do
-                puts "child"
-            end
-            """)
-        out, err = capfd.readouterr()
-        assert err == ""
-        assert out == "child\n"
-        monkeypatch.setattr(os, "fork", lambda: 200)
-        w_res = space.execute("""
-        return Process.fork do
-            puts "child"
-        end
-        """)
-        assert space.int_w(w_res) == 200
-
     def test_waitpid(self, space):
         for code in [0, 1, 173]:
             pid = os.fork()
