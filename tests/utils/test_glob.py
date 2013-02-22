2a6d5a5d37db23f9f52b597d97c6bd702c406c90
Jeremy Thurgood <firxen@gmail.com>
2013-02-22 08:17:22 +0200

    Brackets.

--- tests/utils/test_glob.py
+++ tests/utils/test_glob.py
@@ -42,9 +42,11 @@ class TestGlob(BaseTopazTest):
     def test_non_dotfiles_with_star(self, glob_helper):
         glob_helper.create_paths([
             ".dotfile", ".dotsubdir/.dotfile", ".dotsubdir/nondotfile",
-            "file_one.ext", "file_two.ext", "nondotfile"])
+            "file_one.ext", "file_two.ext", "nondotfile"
+        ])
         assert glob_helper.sglob("*") == [
-            "file_one.ext", "file_two.ext", "nondotfile"]
+            "file_one.ext", "file_two.ext", "nondotfile"
+        ]
         assert glob_helper.sglob("**") == [
             "file_one.ext", "file_two.ext", "nondotfile"]
         assert glob_helper.sglob("*file") == ["nondotfile"]
@@ -52,10 +54,12 @@ class TestGlob(BaseTopazTest):
     def test_dotfiles_with_star(self, glob_helper):
         glob_helper.create_paths([
             ".dotfile", ".dotsubdir/.dotfile", ".dotsubdir/nondotfile",
-            "file_one.ext", "file_two.ext", "nondotfile"])
+            "file_one.ext", "file_two.ext", "nondotfile"
+        ])
         assert glob_helper.sglob(".*") == [".", "..", ".dotfile", ".dotsubdir"]
         assert glob_helper.sglob(".**") == [
-            ".", "..", ".dotfile", ".dotsubdir"]
+            ".", "..", ".dotfile", ".dotsubdir"
+        ]
         assert glob_helper.sglob(".*file") == [".dotfile"]
 
     def test_empty_pattern_no_matches(self, glob_helper):
@@ -64,7 +68,8 @@ class TestGlob(BaseTopazTest):
     def test_regexp_specials(self, glob_helper):
         glob_helper.create_paths([
             "special/+", "special/^", "special/$", "special/(", "special/)",
-            "special/[", "special/]", "special/{", "special/}"])
+            "special/[", "special/]", "special/{", "special/}"
+        ])
         assert glob_helper.glob("special/+") == ["special/+"]
         assert glob_helper.glob("special/^") == ["special/^"]
         assert glob_helper.glob("special/$") == ["special/$"]
@@ -84,7 +89,8 @@ class TestGlob(BaseTopazTest):
     def test_matches_paths_with_globs(self, glob_helper):
         glob_helper.create_paths(["special/test{1}/file[1]"])
         assert glob_helper.glob("special/test\\{1\\}/*") == [
-            "special/test{1}/file[1]"]
+            "special/test{1}/file[1]"
+        ]
 
     def test_dstar_recursion(self, glob_helper):
         glob_helper.create_paths([
@@ -96,16 +102,19 @@ class TestGlob(BaseTopazTest):
             "deeply/nested/directory/structure/.ext",
             "deeply/nested/directory/structure/file_one",
             "deeply/nested/directory/structure/file_one.ext",
-            "deeply/nested/directory/structure/foo", "deeply/nondotfile"])
+            "deeply/nested/directory/structure/foo", "deeply/nondotfile"
+        ])
         assert glob_helper.sglob("**/") == [
             "deeply/", "deeply/nested/", "deeply/nested/directory/",
-            "deeply/nested/directory/structure/", "subdir_one/", "subdir_two/"]
+            "deeply/nested/directory/structure/", "subdir_one/", "subdir_two/"
+        ]
         assert glob_helper.sglob("**/*fil*") == [
             "deeply/nested/directory/structure/file_one",
             "deeply/nested/directory/structure/file_one.ext",
             "deeply/nondotfile", "file_one.ext", "file_two.ext", "nondotfile",
             "subdir_one/nondotfile", "subdir_two/nondotfile",
-            "subdir_two/nondotfile.ext"]
+            "subdir_two/nondotfile.ext"
+        ]
 
     def test_question_mark(self, glob_helper):
         glob_helper.create_paths(["subdir_one", "subdir_two"])
@@ -117,7 +126,8 @@ class TestGlob(BaseTopazTest):
         assert glob_helper.sglob("[stfu]ubdir_one") == ["subdir_one"]
         assert glob_helper.sglob("[A-Za-z]ubdir_one") == ["subdir_one"]
         assert glob_helper.sglob("subdir_[a-z][a-z][a-z]") == [
-            "subdir_one", "subdir_two"]
+            "subdir_one", "subdir_two"
+        ]
 
     def test_negated_character_group(self, glob_helper):
         glob_helper.create_paths(["subdir_one", "subdir_two"])
@@ -130,25 +140,33 @@ class TestGlob(BaseTopazTest):
         glob_helper.create_paths([
             ".dotfile", ".dotsubdir/.dotfile", ".dotsubdir/nondotfile",
             "subdir_one/.dotfile", "subdir_one/nondotfile",
-            "subdir_two/nondotfile", "subdir_two/nondotfile.ext"])
+            "subdir_two/nondotfile", "subdir_two/nondotfile.ext"
+        ])
         assert glob_helper.sglob("subdir_{one,two,three}") == [
-            "subdir_one", "subdir_two"]
+            "subdir_one", "subdir_two"
+        ]
         assert glob_helper.sglob("sub*_{one,two,three}") == [
-            "subdir_one", "subdir_two"]
+            "subdir_one", "subdir_two"
+        ]
         assert glob_helper.sglob("subdir_two/nondotfile{.ext,}") == [
-            "subdir_two/nondotfile", "subdir_two/nondotfile.ext"]
+            "subdir_two/nondotfile", "subdir_two/nondotfile.ext"
+        ]
         assert glob_helper.sglob("{,.}*") == [
-            ".", "..", ".dotfile", ".dotsubdir", "subdir_one", "subdir_two"]
+            ".", "..", ".dotfile", ".dotsubdir", "subdir_one", "subdir_two"
+        ]
 
     def test_braces_ordering(self, glob_helper):
         glob_helper.create_paths([
             "brace/a", "brace/a.js", "brace/a.erb", "brace/a.js.rjs",
-            "brace/a.html.erb"])
+            "brace/a.html.erb"
+        ])
         assert glob_helper.glob("brace/a{.js,.html}{.erb,.rjs}") == [
-            "brace/a.js.rjs", "brace/a.html.erb"]
+            "brace/a.js.rjs", "brace/a.html.erb"
+        ]
         assert glob_helper.glob("brace/a{.{js,html},}{.{erb,rjs},}") == [
             "brace/a.js.rjs", "brace/a.js", "brace/a.html.erb", "brace/a.erb",
-            "brace/a"]
+            "brace/a"
+        ]
 
     def test_escaping(self, glob_helper):
         glob_helper.create_paths(["foo^bar", "nondotfile"])
@@ -158,11 +176,14 @@ class TestGlob(BaseTopazTest):
 
     def test_preserves_separator(self, glob_helper):
         glob_helper.create_paths([
-            "deeply/nested/directory/structure/file_one.ext"])
+            "deeply/nested/directory/structure/file_one.ext"
+        ])
         assert glob_helper.glob("deeply/nested//directory/*/*.ext") == [
-            "deeply/nested//directory/structure/file_one.ext"]
+            "deeply/nested//directory/structure/file_one.ext"
+        ]
         assert glob_helper.glob("deeply/*/directory/structure//**/*.ext") == [
-            "deeply/nested/directory/structure//file_one.ext"]
+            "deeply/nested/directory/structure//file_one.ext"
+        ]
 
     def test_ignores_missing_dirs(self, glob_helper):
         assert glob_helper.glob("deeply/notthere/blah*/whatever") == []
@@ -171,25 +192,32 @@ class TestGlob(BaseTopazTest):
     def test_multiple_recursives(self, glob_helper):
         glob_helper.create_paths(["a/x/b/y/e", "a/x/b/y/b/z/e"])
         assert glob_helper.sglob("a/**/b/**/e") == [
-            "a/x/b/y/b/z/e", "a/x/b/y/e"]
+            "a/x/b/y/b/z/e", "a/x/b/y/e"
+        ]
 
     def test_flag_dotmatch(self, glob_helper):
         glob_helper.create_paths([
             ".dotfile", ".dotsubdir/.dotfile", ".dotsubdir/nondotfile",
             "file_one.ext", "file_two.ext", "nondotfile",
-            "deeply/nested/.dotfile.ext"])
+            "deeply/nested/.dotfile.ext"
+        ])
         assert glob_helper.sglob("*", FNM_DOTMATCH) == [
             ".", "..", ".dotfile", ".dotsubdir", "deeply", "file_one.ext",
-            "file_two.ext", "nondotfile"]
+            "file_two.ext", "nondotfile"
+        ]
         assert glob_helper.sglob("**", FNM_DOTMATCH) == [
             ".", "..", ".dotfile", ".dotsubdir", "deeply", "file_one.ext",
-            "file_two.ext", "nondotfile"]
+            "file_two.ext", "nondotfile"
+        ]
         assert glob_helper.sglob("*file", FNM_DOTMATCH) == [
-            ".dotfile", "nondotfile"]
+            ".dotfile", "nondotfile"
+        ]
         assert glob_helper.sglob("**/", FNM_DOTMATCH) == [
-            ".dotsubdir/", "deeply/", "deeply/nested/"]
+            ".dotsubdir/", "deeply/", "deeply/nested/"
+        ]
         assert glob_helper.sglob("./**/", FNM_DOTMATCH) == [
-            "./", "./.dotsubdir/", "./deeply/", "./deeply/nested/"]
+            "./", "./.dotsubdir/", "./deeply/", "./deeply/nested/"
+        ]
 
     def test_flag_noescape(self, glob_helper):
         # TODO: Skip this on Windows
