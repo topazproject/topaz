5a4b795e238bef7668a88b44d05162efeec8df1d
Jan Stępień <jstepien@users.sourceforge.net>
2013-02-22 18:04:06 +0100

    Revert deletion of our Process.fork tests

--- tests/modules/test_process.py
+++ tests/modules/test_process.py
@@ -34,6 +34,25 @@ class TestProcess(BaseTopazTest):
         """)
         assert self.unwrap(space, w_res) == [False, 1]
 
+    def test_fork(self, space, monkeypatch, capfd):
+        monkeypatch.setattr(os, "fork", lambda: 0)
+        with self.raises(space, "SystemExit"):
+            space.execute("""
+            Process.fork do
+                puts "child"
+            end
+            """)
+        out, err = capfd.readouterr()
+        assert err == ""
+        assert out == "child\n"
+        monkeypatch.setattr(os, "fork", lambda: 200)
+        w_res = space.execute("""
+        return Process.fork do
+            puts "child"
+        end
+        """)
+        assert space.int_w(w_res) == 200
+
     @pytest.mark.parametrize("code", [0, 1, 173])
     def test_waitpid(self, space, code):
         pid = os.fork()
