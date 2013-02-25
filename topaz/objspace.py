e3e049eb90cf239e9768c275449ba5e59b0b9486
Alex Gaynor <alex.gaynor@rd.io>
2013-02-25 08:12:20 -0800

    fix for this logic, always run all handlers

--- topaz/objspace.py
+++ topaz/objspace.py
@@ -559,6 +559,7 @@ class ObjectSpace(object):
         self.exit_handlers_w.append(w_proc)
 
     def run_exit_handlers(self):
+        status = -1
         while self.exit_handlers_w:
             w_proc = self.exit_handlers_w.pop()
             try:
@@ -566,9 +567,10 @@ class ObjectSpace(object):
             except RubyError as e:
                 w_exc = e.w_value
                 if isinstance(w_exc, W_SystemExit):
-                    return w_exc.status
-                print_traceback(self, e.w_value)
-        return -1
+                    status = w_exc.status
+                else:
+                    print_traceback(self, e.w_value)
+        return status
 
     def subscript_access(self, length, w_idx, w_count):
         inclusive = False
