8d4a634aded9cc5261cad3774efe59afbb8c799d
Dan Buch <dan@meatballhat.com>
2013-03-25 23:59:18 -0400

    Meant for this to be the assertion

--- topaz/objects/fileobject.py
+++ topaz/objects/fileobject.py
@@ -105,7 +105,7 @@ class W_FileObject(W_IOObject):
             return space.newstr_fromstr("/")
         if path.endswith("/"):
             newlen = len(path) - 1
-            assert newlen > 1
+            assert newlen >= 0
             path = path[:newlen]
         idx = path.rfind("/")
         while idx > 0 and path[idx - 1] == "/":
