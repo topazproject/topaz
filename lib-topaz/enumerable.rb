c203b84cb1185e843057c975e0e186d75c778e40
Konstantin Makarchev <kostya27@gmail.com>
2013-03-31 00:48:40 +0400

    array#partition

--- lib-topaz/enumerable.rb
+++ lib-topaz/enumerable.rb
@@ -171,4 +171,13 @@ module Enumerable
       end
     end
   end
+
+  def partition(&block)
+    return self.enum_for(:partition) unless block
+    a, b = [], []
+    self.each do |e|
+      block.call(e) ? a.push(e) : b.push(e)
+    end
+    [a, b]
+  end
 end
