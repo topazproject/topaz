93723f55628b91a006e8571d004b8a9bc73071b9
Alex Gaynor <alex.gaynor@rd.io>
2013-04-09 21:57:13 -0700

    Whoops, fix this logic.

--- topaz/interpreter.py
+++ topaz/interpreter.py
@@ -369,7 +369,7 @@ class Interpreter(object):
         w_scope = frame.pop()
 
         name = space.symbol_w(w_name)
-        w_cls = w_scope.find_local_const(space, name)
+        w_cls = w_scope.find_included_const(space, name)
         if w_cls is None:
             if superclass is space.w_nil:
                 superclass = space.w_object
@@ -392,7 +392,7 @@ class Interpreter(object):
         w_scope = frame.pop()
 
         name = space.symbol_w(w_name)
-        w_mod = w_scope.find_local_const(space, name)
+        w_mod = w_scope.find_included_const(space, name)
 
         if w_mod is None:
             w_mod = space.newmodule(name, w_scope=w_scope)
