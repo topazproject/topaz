259d79ed10c33b184d29595cb16343f52183df59
Gerd Mömken <gerd.moemken@uni-duesseldorf.de>
2013-05-03 15:24:44 +0200

    Wrote a new test and implemented it
    However the new implementation definitely needs some refactoring.

--- tests/modules/test_ffi.py
+++ tests/modules/test_ffi.py
@@ -3,6 +3,10 @@ from ..base import BaseTopazTest
 
 class TestFFI(BaseTopazTest):
 
-    def test_function_call(self, space):
+    def test_function_int_arg(self, space):
         w_res = space.execute("FFI.call('abs', -5)")
         assert space.int_w(w_res) == 5
+
+    def test_function_float_arg(self, space):
+        w_res = space.execute("FFI.call('ceil', 1.3)")
+        assert space.float_w(w_res) == 2.0
--- topaz/modules/ffi.py
+++ topaz/modules/ffi.py
@@ -3,17 +3,35 @@ from __future__ import absolute_import
 from topaz.module import ModuleDef, ClassDef
 from topaz.objects.exceptionobject import W_StandardError, new_exception_allocate
 
-from rpython.rlib import clibffi
+from rpython.rlib import clibffi, rarithmetic
 from rpython.rtyper.lltypesystem import rffi
 
 class FFI(object):
     moduledef = ModuleDef("FFI", filepath=__file__)
 
-    @moduledef.function("call", name="str", arg="int")
-    def method_call(self, space, name, arg):
-        assert name == 'abs'
-        clib = clibffi.CDLL(clibffi.get_libc_name())
-        ptr = clib.getpointer(name, [clibffi.ffi_type_sint], clibffi.ffi_type_sint)
+    @moduledef.function("call", name="str")
+    def method_call(self, space, name, w_args):
+        assert name == 'abs' or 'ceil'
+        lib = (clibffi.CDLL('libm.so') if name == 'ceil' else
+               clibffi.CDLL(clibffi.get_libc_name()))
+        if space.is_kind_of(w_args, space.w_integer):
+            arg = space.int_w(w_args)
+            arg_type = clibffi.ffi_type_sint
+            rffi_type = rffi.INT
+        elif space.is_kind_of(w_args, space.w_float):
+            arg = space.float_w(w_args)
+            arg_type = clibffi.ffi_type_double
+            rffi_type = rffi.DOUBLE
+        else:
+            raise Exception("not supported")
+        ptr = lib.getpointer(name, [arg_type], arg_type)
         ptr.push_arg(arg)
-        res = ptr.call(rffi.INT)
-        return space.newint(res)
+        res = ptr.call(rffi_type)
+        #if isinstance(res, rarithmetic.r_int):
+        if name == 'abs':
+            return space.newint(res)
+        elif name == 'ceil':
+        #elif isinstance(res, rarithmetic.r_singlefloat):
+            return space.newfloat(res)
+        else:
+            raise Exception("not supported")
