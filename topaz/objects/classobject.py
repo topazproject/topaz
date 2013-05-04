145fefd62a091e08139dd529f2c93696194def9a
Marjan Krekoten' (Мар'ян Крекотень) <krekoten@gmail.com>
2013-05-04 22:51:16 +0300

    remove superclass from immutable fields

--- topaz/objects/classobject.py
+++ topaz/objects/classobject.py
@@ -6,7 +6,6 @@ from topaz.objects.objectobject import W_Object
 
 
 class W_ClassObject(W_ModuleObject):
-    _immutable_fields_ = ["superclass?"]
 
     classdef = ClassDef("Class", W_ModuleObject.classdef, filepath=__file__)
 
