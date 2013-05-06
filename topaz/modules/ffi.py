a6731a25fa6b29c12bfc9f53b9e08f1109acc3be
Gerd Mömken <gerd.moemken@uni-duesseldorf.de>
2013-05-06 09:00:23 +0200

    Simplified the wrapping part of the call function

--- topaz/modules/ffi.py
+++ topaz/modules/ffi.py
@@ -18,20 +18,15 @@ class FFI(object):
             arg = space.int_w(w_args)
             arg_type = clibffi.ffi_type_sint
             rffi_type = rffi.INT
+            wrap_up = space.newint
         elif space.is_kind_of(w_args, space.w_float):
             arg = space.float_w(w_args)
             arg_type = clibffi.ffi_type_double
             rffi_type = rffi.DOUBLE
+            wrap_up = space.newfloat
         else:
             raise Exception("not supported")
         ptr = lib.getpointer(name, [arg_type], arg_type)
         ptr.push_arg(arg)
         res = ptr.call(rffi_type)
-        #if isinstance(res, rarithmetic.r_int):
-        if name == 'abs':
-            return space.newint(res)
-        elif name == 'ceil':
-        #elif isinstance(res, rarithmetic.r_singlefloat):
-            return space.newfloat(res)
-        else:
-            raise Exception("not supported")
+        return wrap_up(res)
