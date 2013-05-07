f1d9ab558dd10ffb750f217c8048fded5913804b
Marjan Krekoten' (Мар'ян Крекотень) <krekoten@gmail.com>
2013-05-07 09:12:33 +0300

    Send string instead of W_SymbolObject

--- topaz/objects/exceptionobject.py
+++ topaz/objects/exceptionobject.py
@@ -37,7 +37,7 @@ class W_ExceptionObject(W_Object):
         if w_msg is space.w_nil or w_msg is None:
             msg = space.getclass(self).name
         else:
-            msg = space.str_w(space.send(w_msg, space.newsymbol("to_s")))
+            msg = space.str_w(space.send(w_msg, "to_s"))
         self.msg = msg
 
     @classdef.method("to_s")
