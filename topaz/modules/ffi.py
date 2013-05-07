d8a934a1c3d7e4af334beaad682d354e41954604
Gerd Mömken <gerd.moemken@uni-duesseldorf.de>
2013-05-07 11:30:25 +0200

    All types are defined.

--- topaz/modules/ffi.py
+++ topaz/modules/ffi.py
@@ -16,24 +16,29 @@ class FFI(object):
         space.set_const(w_mod, 'Type', space.newclass('Type', None))
         # TODO: LONG and ULONG ommited,
         #       since clibffi doesn't have slong or ulong
+        ffi_type_long = clibffi.cast_type_to_ffitype(rffi.LONG)
+        ffi_type_ulong = clibffi.cast_type_to_ffitype(rffi.ULONG)
         typenames = ['VOID', 'INT8', 'UINT8', 'INT16', 'UINT16', 'INT32',
-                     'UINT32', 'INT64', 'UINT64', 'FLOAT32',
+                     'UINT32', 'INT64', 'UINT64', 'LONG', 'ULONG', 'FLOAT32',
                      'FLOAT64', 'LONGDOUBLE', 'POINTER', 'STRING', 'BUFFER_IN',
                      'BUFFER_OUT', 'BUFFER_INOUT', 'BOOL', 'VARARGS']
         ffitypes = ([clibffi.ffi_type_void, clibffi.ffi_type_sint8,
                      clibffi.ffi_type_uint8, clibffi.ffi_type_sint16,
                      clibffi.ffi_type_uint16, clibffi.ffi_type_sint32,
                      clibffi.ffi_type_uint32, clibffi.ffi_type_sint64,
-                     clibffi.ffi_type_uint64, clibffi.ffi_type_float,
-                     clibffi.ffi_type_double, clibffi.ffi_type_longdouble] +
-                     4*[clibffi.ffi_type_pointer] +
+                     clibffi.ffi_type_uint64, ffi_type_long, ffi_type_ulong,
+                     clibffi.ffi_type_float, clibffi.ffi_type_double,
+                     clibffi.ffi_type_longdouble] +
+                     5*[clibffi.ffi_type_pointer] +
                      [clibffi.ffi_type_uchar, clibffi.ffi_type_void])
+        assert len(typenames) == len(ffitypes)
         typealias = [('INT8', 'SCHAR'), ('INT8', 'CHAR'), ('UINT8', 'UCHAR'),
                      ('INT16', 'SHORT'), ('INT16', 'SSHORT'),
                      ('UINT16', 'USHORT'), ('INT32', 'INT'), ('INT32', 'SINT'),
                      ('UINT32', 'UINT'), ('INT64', 'LONG_LONG'),
-                     ('INT64', 'SLONG_LONG'), ('UINT64', 'ULONG_LONG'),
-                     ('FLOAT32', 'FLOAT'), ('FLOAT64', 'DOUBLE')]
+                     ('LONG', 'SLONG'), ('INT64', 'SLONG_LONG'),
+                     ('UINT64', 'ULONG_LONG'), ('FLOAT32', 'FLOAT'),
+                     ('FLOAT64', 'DOUBLE')]
         for tn in typenames:
             space.set_const(w_mod, 'TYPE_' + tn, space.w_nil)
         rbffi_type_class = space.find_const(w_mod, 'Type')
