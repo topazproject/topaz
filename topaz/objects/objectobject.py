a9296a6b56ae08924c1af8e347a964f9c6307258
Marjan Krekoten' (Мар'ян Крекотень) <krekoten@gmail.com>
2013-05-07 21:26:15 +0300

    BasicObject#instance_exec
    Specs foxed:
    
    BasicObject#instance_exec
    - sets self to the receiver in the context of the passed block
    - passes arguments to the block

--- topaz/objects/objectobject.py
+++ topaz/objects/objectobject.py
@@ -110,6 +110,19 @@ class W_BaseObject(W_Root):
     def method_singleton_method_undefined(self, space, w_name):
         return space.w_nil
 
+    @classdef.method("instance_exec")
+    def method_instance_exec(self, space, args_w, block):
+        if block is None:
+            raise space.error(space.w_LocalJumpError, "no block given")
+        return space.invoke_block(
+            block.copy(
+                space,
+                w_self=self,
+                lexical_scope=StaticScope(space.getsingletonclass(self), None)
+            ),
+            args_w
+        )
+
 
 class W_RootObject(W_BaseObject):
     _attrs_ = []
