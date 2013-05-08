1681135f29a04164228326619c828cfef3c6250b
Gerd Mömken <gerd.moemken@uni-duesseldorf.de>
2013-05-08 10:00:36 +0200

    Added methods to DataConverter
    :native_type, :to_native and :from_native now exist but their bytecodes are
    just mocks. Hence the interpreter raises an Exception because these mocks don't
    even have the correct number of arguments.

--- topaz/modules/ffi.py
+++ topaz/modules/ffi.py
@@ -14,7 +14,20 @@ class FFI(object):
         space.set_const(w_mod, 'TypeDefs', space.newhash())
         space.set_const(w_mod, 'Types', space.newhash())
         space.set_const(w_mod, 'Type', space.newclass('Type', None))
+
         space.set_const(w_mod, 'DataConverter', space.newmodule('DataConverter'))
+        w_dt = space.find_const(w_mod, 'DataConverter')
+        w_mock_bytecode = space.compile("'change me later!'", None)
+        w_func_native_type = space.newfunction(space.newsymbol('native_type'),
+                                               w_mock_bytecode, {})
+        w_func_to_native = space.newfunction(space.newsymbol('to_native'),
+                                             w_mock_bytecode, {})
+        w_func_from_native = space.newfunction(space.newsymbol('from_native'),
+                                               w_mock_bytecode, {})
+        w_dt.define_method(space, 'native_type', w_func_native_type)
+        w_dt.define_method(space, 'to_native', w_func_to_native)
+        w_dt.define_method(space, 'from_native', w_func_from_native)
+
         ffi_type_long = clibffi.cast_type_to_ffitype(rffi.LONG)
         ffi_type_ulong = clibffi.cast_type_to_ffitype(rffi.ULONG)
         ffitypes = {'VOID':clibffi.ffi_type_void,
