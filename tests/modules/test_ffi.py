bf45882ed037fbc2f1b39b2f24b9d58539f7de9b
Gerd Mömken <gerd.moemken@uni-duesseldorf.de>
2013-05-08 10:42:46 +0200

    Started writing tests for the FFI module

--- tests/modules/test_ffi.py
+++ tests/modules/test_ffi.py
@@ -1,12 +1,14 @@
 from ..base import BaseTopazTest
-
+from topaz.modules.ffi import FFI
+from topaz.objects.hashobject import W_HashObject
+from topaz.objects.classobject import W_ClassObject
 
 class TestFFI(BaseTopazTest):
 
-    def test_function_int_arg(self, space):
-        w_res = space.execute("FFI.call('abs', -5)")
-        assert space.int_w(w_res) == 5
-
-    def test_function_float_arg(self, space):
-        w_res = space.execute("FFI.call('ceil', 1.3)")
-        assert space.float_w(w_res) == 2.0
+    def test_basic(self, space):
+        w_type_defs = space.execute('FFI::TypeDefs')
+        assert isinstance(w_type_defs, W_HashObject)
+        w_types = space.execute('FFI::Types')
+        assert isinstance(w_types, W_HashObject)
+        w_type = space.execute('FFI::Type')
+        assert isinstance(w_type, W_ClassObject)
