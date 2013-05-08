e82e7c9188e50d127d46c03dc39a793d3eb97f5d
Gerd Mömken <gerd.moemken@uni-duesseldorf.de>
2013-05-08 11:03:34 +0200

    Added tests for primitive types

--- tests/modules/test_ffi.py
+++ tests/modules/test_ffi.py
@@ -5,10 +5,24 @@ from topaz.objects.classobject import W_ClassObject
 
 class TestFFI(BaseTopazTest):
 
+    primitive_types =  ['INT8', 'UINT8', 'INT16', 'UINT16',
+                        'INT32', 'UINT32', 'INT64', 'UINT64',
+                        'LONG', 'ULONG', 'FLOAT32', 'FLOAT64',
+                        'VOID', 'LONGDOUBLE', 'POINTER', 'BOOL']
+
     def test_basic(self, space):
         w_type_defs = space.execute('FFI::TypeDefs')
         assert isinstance(w_type_defs, W_HashObject)
         w_types = space.execute('FFI::Types')
         assert isinstance(w_types, W_HashObject)
+
+    def test_FFI_type_constants(self, space):
+        # just check, whether the constants even exist for now
+        for pt in TestFFI.primitive_types:
+            space.execute('FFI::TYPE_%s' % pt)
+
+    def test_Type(self, space):
         w_type = space.execute('FFI::Type')
         assert isinstance(w_type, W_ClassObject)
+        for pt in TestFFI.primitive_types:
+            w_type.find_const(space, space.newsymbol(pt))
