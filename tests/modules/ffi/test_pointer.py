48baddb2c6a3aa90606f046de98d37895e171f3a
Gerd Mömken <gerd.moemken@uni-duesseldorf.de>
2013-05-16 15:47:21 +0200

    Defined all required methods Pointer class
    In the case of the autorelease methods the implementation was
    obvious so they are implemented correctly right away.
    All the other methods were not that trivial so they just have mock
    return values for now.

--- tests/modules/ffi/test_pointer.py
+++ tests/modules/ffi/test_pointer.py
@@ -7,3 +7,31 @@ class TestPointer(BaseTopazTest):
         assert self.unwrap(space, w_res)
         w_res = space.execute("FFI::Pointer::NULL.null?")
         assert self.unwrap(space, w_res)
+
+    def test_methods_exist(self, space):
+        space.execute("FFI::Pointer::NULL.address")
+        space.execute("FFI::Pointer::NULL + FFI::Pointer::NULL")
+        space.execute("FFI::Pointer::NULL.slice(0, 5)")
+        with self.raises(space, "TypeError", "can't convert String into Integer"):
+            space.execute("FFI::Pointer::NULL.slice('foo', 5)")
+            space.execute("FFI::Pointer::NULL.slice(0, 'bar')")
+        w_res = space.execute("FFI::Pointer::NULL.to_i == "
+                              "FFI::Pointer::NULL.address")
+        assert self.unwrap(space, w_res) # to_i is just an alias for address
+        w_res = space.execute("FFI::Pointer::NULL == FFI::Pointer::NULL")
+        assert self.unwrap(space, w_res)
+        space.execute("FFI::Pointer::NULL.order(:big)")
+        with self.raises(space, "TypeError", "42 is not a symbol"):
+            space.execute("FFI::Pointer::NULL.order(42)")
+        space.execute("FFI::Pointer::NULL.free")
+        space.execute("FFI::Pointer::NULL.type_size")
+
+    def test_autorelease(self, space):
+        w_res = space.execute("FFI::Pointer::NULL.autorelease=(true)")
+        assert self.unwrap(space, w_res)
+        w_res = space.execute("FFI::Pointer::NULL.autorelease?")
+        assert self.unwrap(space, w_res)
+        w_res = space.execute("FFI::Pointer::NULL.autorelease=(false)")
+        assert not self.unwrap(space, w_res)
+        w_res = space.execute("FFI::Pointer::NULL.autorelease?")
+        assert not self.unwrap(space, w_res)
--- topaz/modules/ffi/pointer.py
+++ topaz/modules/ffi/pointer.py
@@ -16,3 +16,42 @@ class W_PointerObject(W_Object):
     def method_null_p(self, space):
         w_null_instance = space.find_const(self.getclass(space), 'NULL')
         return space.newbool(self is w_null_instance)
+
+    @classdef.method('address')
+    def method_address(self, space):
+        return space.newint(0)
+
+    @classdef.method('+')
+    def method_plus(self, space, w_other):
+        return space.newint(0)
+
+    # TODO: actually all Numerics should be accepted
+    @classdef.method('slice', offset='int', length='int')
+    def method_address(self, space, offset, length):
+        return space.newint(0)
+
+    @classdef.method('to_i')
+    def method_to_i(self, space):
+        return space.newint(0)
+
+    @classdef.method('order', endianness='symbol')
+    def method_order(self, space, endianness):
+        return space.newint(0)
+
+    @classdef.method('autorelease=', val='bool')
+    def method_autorelease_eq(self, space, val):
+        self.autorelease = val
+        return space.newbool(val)
+
+    @classdef.method('autorelease?')
+    def method_autorelease_p(self, space):
+        return space.newbool(self.autorelease)
+
+    @classdef.method('free')
+    def method_free(self, space):
+        # TODO: Free stuff self is pointing at here
+        return self
+
+    @classdef.method('type_size')
+    def method_type_size(self, space):
+        return space.newint(0)
