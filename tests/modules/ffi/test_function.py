2f135a23600f3895c3b1812a64e81ed5cf7d430a
Gerd Mömken <gerd.moemken@uni-duesseldorf.de>
2013-05-28 17:44:09 +0200

    Started input checking Function.initialize
    It's not just about the type checking, it's also that for example
    :int8 will be accepted as a replacement for FFI::Type::INT8.

--- tests/modules/ffi/test_function.py
+++ tests/modules/ffi/test_function.py
@@ -3,6 +3,15 @@ from topaz.objects.classobject import W_ClassObject
 
 class TestFunction(BaseTopazTest):
 
-    def test_basic(self, space):
-        w_function = space.execute("FFI::Function")
-        assert isinstance(w_function, W_ClassObject)
+    def test_initialize(self, space):
+        w_function = space.execute("""
+        FFI::Function.new(:void, [:int8, :int16], {})
+        """) #didn't crash
+        w_function = space.execute("""
+        FFI::Function.new(FFI::Type::VOID,
+                          [FFI::Type::INT8, FFI::Type::INT16], {})
+        """) # didn't crash
+        with self.raises(space, "TypeError", "can't convert Fixnum into Type"):
+            space.execute("FFI::Function.new(1, [], {})")
+        with self.raises(space, "TypeError", "can't convert Symbol into Type"):
+            space.execute("FFI::Function.new(:null, [], {})")
--- topaz/modules/ffi/function.py
+++ topaz/modules/ffi/function.py
@@ -1,5 +1,25 @@
 from topaz.objects.objectobject import W_Object
 from topaz.module import ClassDef
+from topaz.modules.ffi.type import W_TypeObject
 
 class W_FunctionObject(W_Object):
     classdef = ClassDef('Function', W_Object.classdef)
+
+    @classdef.singleton_method('allocate')
+    def singleton_method_allocate(self, space, args_w):
+        return W_FunctionObject(space)
+
+    @classdef.method('initialize')
+    def method_initialize(self, space, w_ret_type, w_arg_types, w_options):
+        if not space.is_kind_of(w_ret_type, space.getclassfor(W_TypeObject)):
+            if space.is_kind_of(w_ret_type, space.w_symbol):
+                sym = space.symbol_w(w_ret_type)
+                if sym.upper() in W_TypeObject.basics:
+                    pass
+                else:
+                    raise space.error(space.w_TypeError,
+                                      "can't convert Symbol into Type")
+            else:
+                tp = w_ret_type.getclass(space).name
+                raise space.error(space.w_TypeError,
+                                  "can't convert %s into Type" % tp)
