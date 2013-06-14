9aafb9fb9790f718b8f40b33f49b32e17cf91824
Gerd Mömken <gerd.moemken@uni-duesseldorf.de>
2013-06-14 16:32:59 +0200

    Added all types to initialize Buffer

--- tests/modules/ffi/test_buffer.py
+++ tests/modules/ffi/test_buffer.py
@@ -1,11 +1,27 @@
 from tests.base import BaseTopazTest
-from rpython.rtyper.lltypesystem import rffi
+from topaz.modules.ffi.buffer import W_BufferObject
+from rpython.rtyper.lltypesystem.rffi import sizeof, LONG, ULONG
 
 class TestBuffer(BaseTopazTest):
+
+    sizes = {'char': 1,
+                   'uchar': 1,
+                   'short': 2,
+                   'ushort': 2,
+                   'int': 4,
+                   'uint': 4,
+                   'long': sizeof(LONG),
+                   'ulong': sizeof(ULONG),
+                   'long_long': 8,
+                   'ulong_long': 8,
+                   'float': 4,
+                   'double': 8}
+
     def test_initialize(self, space):
-        w_res = space.execute("""
-        buffer = FFI::Buffer.new(:int, 3)
-        buffer.total
-        """)
-        expected = rffi.sizeof(rffi.INT)*3
-        assert self.unwrap(space, w_res) == expected
+        for key in TestBuffer.sizes:
+            w_res = space.execute("""
+            buffer = FFI::Buffer.new(:%s, 3)
+            buffer.total
+            """ % key)
+            expected = TestBuffer.sizes[key]*3
+            assert self.unwrap(space, w_res) == expected
--- topaz/modules/ffi/buffer.py
+++ topaz/modules/ffi/buffer.py
@@ -1,8 +1,22 @@
 from topaz.objects.objectobject import W_Object
 from topaz.module import ClassDef
+from rpython.rtyper.lltypesystem import rffi
 
 class W_BufferObject(W_Object):
     classdef = ClassDef('Buffer', W_Object.classdef)
+
+    typesymbols = {'char': rffi.CHAR,
+                   'uchar': rffi.CHAR,
+                   'short': rffi.SHORT,
+                   'ushort': rffi.SHORT,
+                   'int': rffi.INT,
+                   'uint': rffi.INT,
+                   'long': rffi.LONG,
+                   'ulong': rffi.ULONG,
+                   'long_long': rffi.LONGLONG,
+                   'ulong_long': rffi.ULONGLONG,
+                   'float': rffi.FLOAT,
+                   'double': rffi.DOUBLE}
 
     @classdef.setup_class
     def setup_class(cls, space, w_cls):
@@ -14,5 +28,5 @@ class W_BufferObject(W_Object):
 
     @classdef.method('initialize', typesym='symbol', length='int')
     def method_initialize(self, space, typesym, length):
-        size = {'int': 4}[typesym]
+        size = rffi.sizeof(self.typesymbols[typesym])
         self.set_instance_var(space, '@total', space.newint(length * size))
