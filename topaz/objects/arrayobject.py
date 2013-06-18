f5d931d212b31f302e6b6537f3c8350200d2bf52
Alex Gaynor <alex.gaynor@gmail.com>
2013-06-17 20:00:49 -0700

    today has not been a good day for me

--- topaz/objects/arrayobject.py
+++ topaz/objects/arrayobject.py
@@ -56,7 +56,7 @@ class W_ArrayObject(W_Object):
         return self.items_w
 
     def length(self):
-        return self.length()
+        return len(self.items_w)
 
     @classdef.singleton_method("allocate")
     def singleton_method_allocate(self, space):
