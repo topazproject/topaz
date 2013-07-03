3693477437e0d87a7ffa1da74e99eda16f5d906d
Gerd Mömken <gerd.moemken@uni-duesseldorf.de>
2013-07-03 15:30:27 +0200

    Starting on MemoryPointer implementation
    Implementing it as a Buffer wrapper. MemoryPointer#initialize tries to make a
    Buffer object out of the first arg.

--- tests/modules/ffi/test_memory_pointer.py
+++ tests/modules/ffi/test_memory_pointer.py
@@ -1,4 +1,5 @@
 from tests.base import BaseTopazTest
+from topaz.modules.ffi.buffer import W_BufferObject
 
 class TestMemoryPointer(BaseTopazTest):
 
@@ -6,3 +7,9 @@ class TestMemoryPointer(BaseTopazTest):
         question = "FFI::MemoryPointer.superclass.equal? FFI::Pointer"
         w_answer = space.execute(question)
         assert self.unwrap(space, w_answer)
+
+    def test_makes_a_buffer_out_of_1st_arg(self, space):
+        w_pointer = space.execute("FFI::MemoryPointer.new(:int)")
+        w_buffer = space.find_instance_var(w_pointer, '@buffer')
+        assert w_buffer.getclass(space) is space.getclassfor(W_BufferObject)
+
--- topaz/modules/ffi/memory_pointer.py
+++ topaz/modules/ffi/memory_pointer.py
@@ -1,7 +1,18 @@
 from topaz.modules.ffi.pointer import W_PointerObject
+from topaz.modules.ffi.buffer import W_BufferObject
 from topaz.module import ClassDef
 
 from rpython.rtyper.lltypesystem import rffi
 
 class W_MemoryPointerObject(W_PointerObject):
     classdef = ClassDef('MemoryPointer', W_PointerObject.classdef)
+
+    @classdef.singleton_method('allocate')
+    def singleton_method_allocate(self, space, args_w):
+        return W_MemoryPointerObject(space)
+
+    @classdef.method('initialize')
+    def method_initialize(self, space, w_sym):
+        w_buffer_cls = space.getclassfor(W_BufferObject)
+        w_buffer = space.send(w_buffer_cls, 'new', [w_sym])
+        self.set_instance_var(space, '@buffer', w_buffer)
