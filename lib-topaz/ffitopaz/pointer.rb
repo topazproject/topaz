8b517c2e090ae59667aeb9e923dc373beb8af5d8
Gerd Mömken <gerd.moemken@uni-duesseldorf.de>
2013-07-05 12:30:47 +0200

    NULL raises NullPointerError on read/write attempt
    ... But only if the read/write method is missing.

--- lib-topaz/ffitopaz/pointer.rb
+++ lib-topaz/ffitopaz/pointer.rb
@@ -3,5 +3,15 @@ module FFI
     def NULL.==(other)
       other.equal? nil
     end
+    def NULL.method_missing(meth_id, *args)
+      meth_name = meth_id.id2name
+      if meth_name.start_with? 'read'
+        raise NullPointerError.new("read attempt on NULL pointer")
+      end
+      if meth_name.start_with? 'write'
+        raise NullPointerError.new("write attempt on NULL pointer")
+      end
+      raise NoMethodError.new("undefined method `#{meth_name}' for #{self}")
+    end
   end
 end
