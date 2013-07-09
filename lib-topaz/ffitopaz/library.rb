f4eade8a20f7bae0732f8957329c15b45bf9922b
Gerd Mömken <gerd.moemken@uni-duesseldorf.de>
2013-07-09 11:23:02 +0200

    Refactored FFI::Library#method_missing

--- lib-topaz/ffitopaz/library.rb
+++ lib-topaz/ffitopaz/library.rb
@@ -2,11 +2,8 @@ module FFI
   module Library
     Attachments = {}
     def self.method_missing(meth_id, *args)
-      if const_defined?(:Attachments) and Attachments.include? meth_id
-        return Attachments[meth_id].call(*args)
-      else
-        raise NoMethodError.new("undefined method `#{meth_id}' for #{self}")
-      end
+      return Attachments[meth_id].call(*args) if Attachments.include? meth_id
+      raise NoMethodError.new("undefined method `#{meth_id}' for #{self}")
     end
   end
 end
