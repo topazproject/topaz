598365f510b0237fa06dd488633a8a4554cab94c
Alex Gaynor <alex.gaynor@gmail.com>
2013-07-11 18:23:40 +1000

    Revert "this passes nowadays"
    This reverts commit 21707aa374ab2587be211f8c71cdd0c665d92f98.

--- tests/objects/test_ioobject.py
+++ tests/objects/test_ioobject.py
@@ -334,6 +334,7 @@ class TestIO(BaseTopazTest):
         """)
         assert self.unwrap(space, w_res) == [True, "foo\n"]
 
+    @pytest.mark.xfail
     def test_popen_write(self, space, capfd):
         space.execute("""
         IO.popen("cat", "w") do |io|
