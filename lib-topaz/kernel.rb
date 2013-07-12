103481cf22a58d63c57df01808635f889d4c761f
Philipp Fehre <philipp.fehre@googlemail.com>
2013-07-12 21:44:48 +0200

    Add Kernel#chop

--- lib-topaz/kernel.rb
+++ lib-topaz/kernel.rb
@@ -23,6 +23,11 @@ module Kernel
     self == other ? 0 : nil
   end
 
+  def chop
+    $_.chop!
+  end
+  private :chop
+
   def Array(arg)
     if ary = Topaz.try_convert_type(arg, Array, :to_ary)
       ary
