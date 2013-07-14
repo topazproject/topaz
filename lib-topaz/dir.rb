729a523367cb463ea7d6dc60a50da61c24ec994e
Alex Gaynor <alex.gaynor@gmail.com>
2013-07-13 17:46:42 -0700

    more readable

--- lib-topaz/dir.rb
+++ lib-topaz/dir.rb
@@ -1,18 +1,18 @@
 class Dir
-
   def self.exists?(dirname)
     if dirname.respond_to?(:to_path)
       dirname = dirname.to_path
     end
     begin
-      !!new(dirname)
+      new(dirname)
     rescue Errno::ENOENT, Errno::ENOTDIR
-      false
+      return false
+    else
+      return true
     end
   end
 
   class << self
     alias :exist? :exists?
   end
-
 end
