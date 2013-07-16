249a405b63b8cdcd1c45d6e3e140c92e8bb93641
Gerd Mömken <gerd.moemken@uni-duesseldorf.de>
2013-07-16 17:32:47 +0200

    Refactoring types

--- topaz/modules/ffi/type.py
+++ topaz/modules/ffi/type.py
@@ -24,6 +24,26 @@ ffi_types = {'VOID':clibffi.ffi_type_void,
              'BOOL': clibffi.ffi_type_uchar,
              'VARARGS': clibffi.ffi_type_void}
 
+ffi_types['SCHAR'] == ffi_types['INT8']
+ffi_types['CHAR'] == ffi_types['INT8']
+ffi_types['UCHAR'] == ffi_types['UINT8']
+ffi_types['SHORT'] == ffi_types['INT16']
+ffi_types['SSHORT'] == ffi_types['INT16']
+ffi_types['UHORT'] == ffi_types['UINT16']
+ffi_types['INT'] == ffi_types['INT32']
+ffi_types['SINT'] == ffi_types['INT32']
+ffi_types['UINT'] == ffi_types['UINT32']
+ffi_types['LONG_LONG'] == ffi_types['INT64']
+ffi_types['SLONG'] == ffi_types['LONG']
+ffi_types['SLONG_LONG'] == ffi_types['INT64']
+ffi_types['ULONG_LONG'] == ffi_types['UINT64']
+ffi_types['FLOAT'] == ffi_types['FLOAT32']
+ffi_types['DOUBLE'] == ffi_types['FLOAT64']
+ffi_types['STRING'] == ffi_types['POINTER']
+ffi_types['BUFFER_IN'] == ffi_types['POINTER']
+ffi_types['BUFFER_OUT'] == ffi_types['POINTER']
+ffi_types['BUFFER_INOUT'] == ffi_types['POINTER']
+
 native_types = {'VOID': rffi.VOIDP,
                 'INT8': rffi.CHAR,
                 'UINT8': rffi.UCHAR,
@@ -38,29 +58,49 @@ native_types = {'VOID': rffi.VOIDP,
                 'FLOAT32': rffi.FLOAT,
                 'FLOAT64': rffi.DOUBLE,
                 'LONGDOUBLE': rffi.LONGDOUBLE,
-                'POINTER': rffi.LONGLONG,
+                'POINTER': rffi.LONG,
                 'BOOL': rffi.CHAR,
                 'VARARGS': rffi.CHAR}
 
-aliases = {'SCHAR': 'INT8',
-           'CHAR': 'INT8',
-           'UCHAR': 'UINT8',
-           'SHORT': 'INT16',
-           'SSHORT': 'INT16',
-           'USHORT': 'UINT16',
-           'INT': 'INT32',
-           'SINT': 'INT32',
-           'UINT': 'UINT32',
-           'LONG_LONG': 'INT64',
-           'SLONG': 'LONG',
-           'SLONG_LONG': 'INT64',
-           'ULONG_LONG': 'UINT64',
-           'FLOAT': 'FLOAT32',
-           'DOUBLE': 'FLOAT64',
-           'STRING': 'POINTER',
-           'BUFFER_IN': 'POINTER',
-           'BUFFER_OUT': 'POINTER',
-           'BUFFER_INOUT': 'POINTER'}
+native_types['SCHAR'] == native_types['INT8']
+native_types['CHAR'] == native_types['INT8']
+native_types['UCHAR'] == native_types['UINT8']
+native_types['SHORT'] == native_types['INT16']
+native_types['SSHORT'] == native_types['INT16']
+native_types['UHORT'] == native_types['UINT16']
+native_types['INT'] == native_types['INT32']
+native_types['SINT'] == native_types['INT32']
+native_types['UINT'] == native_types['UINT32']
+native_types['LONG_LONG'] == native_types['INT64']
+native_types['SLONG'] == native_types['LONG']
+native_types['SLONG_LONG'] == native_types['INT64']
+native_types['ULONG_LONG'] == native_types['UINT64']
+native_types['FLOAT'] == native_types['FLOAT32']
+native_types['DOUBLE'] == native_types['FLOAT64']
+native_types['STRING'] == native_types['POINTER']
+native_types['BUFFER_IN'] == native_types['POINTER']
+native_types['BUFFER_OUT'] == native_types['POINTER']
+native_types['BUFFER_INOUT'] == native_types['POINTER']
+
+#aliases = {'SCHAR': 'INT8',
+#           'CHAR': 'INT8',
+#           'UCHAR': 'UINT8',
+#           'SHORT': 'INT16',
+#           'SSHORT': 'INT16',
+#           'USHORT': 'UINT16',
+#           'INT': 'INT32',
+#           'SINT': 'INT32',
+#           'UINT': 'UINT32',
+#           'LONG_LONG': 'INT64',
+#           'SLONG': 'LONG',
+#           'SLONG_LONG': 'INT64',
+#           'ULONG_LONG': 'UINT64',
+#           'FLOAT': 'FLOAT32',
+#           'DOUBLE': 'FLOAT64',
+#           'STRING': 'POINTER',
+#           'BUFFER_IN': 'POINTER',
+#           'BUFFER_OUT': 'POINTER',
+#           'BUFFER_INOUT': 'POINTER'}
 
 class W_TypeObject(W_Object):
     classdef = ClassDef('Type', W_Object.classdef)
@@ -71,9 +111,6 @@ class W_TypeObject(W_Object):
         for typename in ffi_types:
             w_new_builtin_type = W_BuiltinObject(space, typename)
             space.set_const(w_cls, typename, w_new_builtin_type)
-        for aka in aliases:
-            w_new_type = W_TypeObject(space, aka)
-            space.set_const(w_cls, aka, w_new_type)
         space.set_const(w_cls, 'Mapped', space.getclassfor(W_MappedObject))
         space.set_const(w_cls, 'Builtin', space.getclassfor(W_BuiltinObject))
 
@@ -91,17 +128,11 @@ class W_TypeObject(W_Object):
             return False
         return self.get_name() == other.get_name()
 
-    def get_name(self):
-        return aliases[self.name]
-
-    def get_native_type(self):
-        return native_types[self.get_name()]
-
-    def get_ffi_type(self):
-        return ffi_types[self.get_name()]
-
     def get_native_type(self):
         return native_types[self.name]
+
+    def get_ffi_type(self):
+        return ffi_types[self.name]
 
     @classdef.method('size')
     def method_size(self, space):
@@ -109,11 +140,11 @@ class W_TypeObject(W_Object):
         size = intmask(r_uint_size)
         return space.newint(size)
 
-class W_BuiltinObject(W_TypeObject):
-    classdef = ClassDef('Builtin', W_TypeObject.classdef)
-
-    def get_name(self):
-        return self.name
+#class W_BuiltinObject(W_TypeObject):
+#    classdef = ClassDef('Builtin', W_TypeObject.classdef)
+#
+#    def get_name(self):
+#        return self.name
 
 class W_MappedObject(W_Object):
     classdef = ClassDef('MappedObject', W_Object.classdef)
