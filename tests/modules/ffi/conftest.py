8075734ee66d784062d37e8d7da4f7d85b772172
Gerd Mömken <gerd.moemken@uni-duesseldorf.de>
2013-07-25 10:47:17 +0200

    Made pytest_funcarg__libtest_so a bit safer

--- tests/modules/ffi/conftest.py
+++ tests/modules/ffi/conftest.py
@@ -21,8 +21,8 @@ def pytest_funcarg__ffis(request, space):
 def pytest_funcarg__libtest_so():
     self_dir = os.path.join(os.path.dirname(__file__))
     rel_to_makefile = os.path.join('libtest', 'GNUmakefile')
-    makefile = os.path.join(self_dir, rel_to_makefile)
+    makefile = os.path.abspath(os.path.join(self_dir, rel_to_makefile))
     os.system("make -f " + makefile)
     rel_to_libtest_so = os.path.join('build', 'libtest.so')
-    libtest_so = os.path.join(self_dir, rel_to_libtest_so)
+    libtest_so = os.path.abspath(os.path.join(self_dir, rel_to_libtest_so))
     return libtest_so
