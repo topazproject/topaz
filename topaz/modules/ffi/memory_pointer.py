ce0ca696077ff1ca65eed3c4ff25509f9adc0652
Gerd Mömken <gerd.moemken@uni-duesseldorf.de>
2013-07-26 10:29:15 +0200

    This will probably make RPython happier

--- topaz/modules/ffi/memory_pointer.py
+++ topaz/modules/ffi/memory_pointer.py
@@ -1,12 +1,17 @@
 from topaz.modules.ffi.pointer import W_PointerObject
 from topaz.modules.ffi.buffer import W_BufferObject
 from topaz.module import ClassDef
-from topaz.modules.ffi.type import type_object
+from topaz.modules.ffi.type import W_TypeObject, type_object
 
 from rpython.rtyper.lltypesystem import rffi
 
 class W_MemoryPointerObject(W_PointerObject):
     classdef = ClassDef('MemoryPointer', W_PointerObject.classdef)
+
+    def __init__(self, space):
+        W_PointerObject.__init__(self, space)
+        self.w_type = W_TypeObject(space, 'DUMMY')
+        self.content = []
 
     @classdef.singleton_method('allocate')
     def singleton_method_allocate(self, space, args_w):
