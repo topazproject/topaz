0dbce1bade220d4dea2524d303d8ea088f1d4350
Gerd Mömken <gerd.moemken@uni-duesseldorf.de>
2013-07-26 10:23:04 +0200

    Starting to work on MemoryPointer
    Implemented the initialize method

--- tests/modules/ffi/test_memory_pointer.py
+++ tests/modules/ffi/test_memory_pointer.py
@@ -7,26 +7,11 @@ class TestMemoryPointer(BaseFFITest):
         assert self.ask(space,
         "FFI::MemoryPointer.ancestors.include? FFI::Pointer")
 
-    def test_it_makes_a_buffer_out_of_1st_arg(self, space):
-        w_res = space.execute("""
-        class FFI::MemoryPointer
-            attr_reader :buffer
-        end
-        FFI::MemoryPointer.new(:int, 3).buffer.total
-        """)
-        assert self.unwrap(space, w_res) == 12
+class TestMemoryPointer__new(BaseFFITest):
+    def test_it_sets_up_a_wrapped_type_object(self, ffis):
+        w_mem_ptr = ffis.execute("FFI::MemoryPointer.new(:int32, 1)")
+        assert w_mem_ptr.w_type == ffis.execute("FFI::Type::INT32")
 
-    def test_it_delegates_to_buffer_in_method_missing(self, space):
-        w_res = space.execute("""
-        class FFI::MemoryPointer
-            public :method_missing
-        end
-        class FFI::Buffer
-            def mock_method
-                'as expected'
-            end
-        end
-        mem_ptr = FFI::MemoryPointer.new(:char)
-        mem_ptr.method_missing(:mock_method)
-        """)
-        assert self.unwrap(space, w_res) == 'as expected'
+    def test_it_sets_up_a_fixed_size_list(self, ffis):
+        w_mem_ptr = ffis.execute("FFI::MemoryPointer.new(:int8, 5)")
+        assert w_mem_ptr.content == [0]*5
