c9e8be17e1fa18c00fcdeee1c87360e1d7d20a8f
Gerd Mömken <gerd.moemken@uni-duesseldorf.de>
2013-09-12 15:24:09 +0200

    Refactored the conftest: creating ffis
    1) Using less deepcopies (only one, to be precise)
    2) executing 'require ffi' after the deepcopy

--- tests/modules/ffi/conftest.py
+++ tests/modules/ffi/conftest.py
@@ -1,33 +1,16 @@
 import copy
 import os
 
-def pytest_funcarg__ffis(request, space):
-    def build_ffis():
-        ffis = copy.deepcopy(space)
-        system, _, _, _, cpu = os.uname() # not for windows
-        ffis.execute("""
-        RUBY_ENGINE = 'topaz'
-        RUBY_PLATFORM = '%s-%s'
-        load 'ffi.rb'
-        """ % (cpu, system.lower()))
-        return ffis
+from rpython.config.translationoption import get_combined_translation_config
 
-    ffis = request.cached_setup(
-        setup=build_ffis,
-        scope="session",
-    )
-    ffis.execute("""
-    class Symbol
-        def eql?(other)
-            self.to_s == other.to_s
-        end
-        def hash
-            self.to_s.hash - 10000
-        end
-    end
-    """)
-    cp = copy.deepcopy(ffis)
-    return cp
+def pytest_funcarg__ffis(request, space):
+    system, _, _, _, cpu = os.uname() # not for windows
+    space.execute("""
+    RUBY_ENGINE = 'topaz'
+    RUBY_PLATFORM = '%s-%s'
+    load 'ffi.rb'
+    """ % (cpu, system.lower()))
+    return space
 
 def pytest_funcarg__libtest_so():
     self_dir = os.path.join(os.path.dirname(__file__))
