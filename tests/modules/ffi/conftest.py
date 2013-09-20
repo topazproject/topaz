6b2e97007b039e99621b0f108df0c010e4d4a371
David Schneider <david.schneider@bivab.de>
2013-09-20 14:23:32 +0200

    refactor libtest_so funcarg

--- tests/modules/ffi/conftest.py
+++ tests/modules/ffi/conftest.py
@@ -1,4 +1,6 @@
 import os
+import subprocess
+import sys
 
 from rpython.rtyper.lltypesystem.ll2ctypes import ALLOCATED
 
@@ -12,10 +14,10 @@ def pytest_funcarg__ffis(request, space):
     return space
 
 def pytest_funcarg__libtest_so():
+    ext = 'dylib' if sys.platform == 'darwin' else 'so'
     self_dir = os.path.join(os.path.dirname(__file__))
-    rel_to_makefile = os.path.join('libtest', 'GNUmakefile')
-    makefile = os.path.abspath(os.path.join(self_dir, rel_to_makefile))
-    os.system("make -f " + makefile)
-    rel_to_libtest_so = os.path.join('build', 'libtest.so')
+    makefile = os.path.join('libtest', 'GNUmakefile')
+    subprocess.call(["make" , "-f", makefile], cwd=self_dir)
+    rel_to_libtest_so = os.path.join('build', 'libtest.' + ext)
     libtest_so = os.path.abspath(os.path.join(self_dir, rel_to_libtest_so))
     return libtest_so
