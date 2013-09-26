e8ac34685bad24976619e853ef9a706c83c3f024
Gerd Mömken <gerd.moemken@uni-duesseldorf.de>
2013-09-26 15:26:29 +0200

    function now also uses _ruby_wrap_llval

--- topaz/modules/ffi/function.py
+++ topaz/modules/ffi/function.py
@@ -6,6 +6,9 @@ from topaz.modules.ffi import type as ffitype
 from topaz.modules.ffi.dynamic_library import (W_DL_SymbolObject,
                                                coerce_dl_symbol)
 from topaz.modules.ffi.pointer import W_PointerObject, coerce_pointer
+from topaz.modules.ffi._ruby_wrap_llval import (_ruby_wrap_number,
+                                                _ruby_wrap_POINTER,
+                                                _ruby_wrap_STRING)
 from topaz.error import RubyError
 from topaz.coerce import Coerce
 from topaz.objects.functionobject import W_BuiltinFunction
@@ -20,8 +23,6 @@ from rpython.rlib.jit_libffi import jit_ffi_call
 from rpython.rlib.jit_libffi import jit_ffi_prep_cif
 from rpython.rlib.unroll import unrolling_iterable
 from rpython.rlib.objectmodel import specialize
-from rpython.rlib.rarithmetic import intmask, longlongmask
-from rpython.rlib.rbigint import rbigint
 from rpython.rlib.objectmodel import we_are_translated
 
 # XXX maybe move to rlib/jit_libffi
@@ -188,24 +189,24 @@ class W_FunctionObject(W_PointerObject):
         # XXX refactor
         if t == FLOAT64 or t == FLOAT32:
             result = misc.read_raw_float_data(data, typesize)
-            return self._ruby_wrap_number(space, result, t)
+            return _ruby_wrap_number(space, result, t)
         elif t == LONG or t == INT64 or t == INT32 or t == INT16 or t == INT8:
             result = misc.read_raw_signed_data(data, typesize)
-            return self._ruby_wrap_number(space, result, t)
+            return _ruby_wrap_number(space, result, t)
         elif t == BOOL:
             result = bool(misc.read_raw_signed_data(data, typesize))
-            return self._ruby_wrap_number(space, result, t)
+            return _ruby_wrap_number(space, result, t)
         elif t == ULONG or t == UINT64 or t == UINT32 or t == UINT16 or t == UINT8:
             result = misc.read_raw_unsigned_data(data, typesize)
-            return self._ruby_wrap_number(space, result, t)
+            return _ruby_wrap_number(space, result, t)
         elif t == STRING:
             result = misc.read_raw_unsigned_data(data, typesize)
             result = rffi.cast(rffi.CCHARP, result)
-            return self._ruby_wrap_STRING(space, result)
+            return _ruby_wrap_STRING(space, result)
         elif t == POINTER:
             result = misc.read_raw_unsigned_data(data, typesize)
             result = rffi.cast(rffi.VOIDP, result)
-            return self._ruby_wrap_POINTER(space, result)
+            return _ruby_wrap_POINTER(space, result)
         raise Exception("Bug in FFI: unknown Type %s" % ffitype.type_names[t])
 
     def _convert_to_NULL_if_nil(self, space, w_arg):
@@ -255,45 +256,6 @@ class W_FunctionObject(W_PointerObject):
                 else:
                     assert 0
         return
-
-    @specialize.arg(3)
-    def _ruby_wrap_number(self, space, res, restype):
-        if restype == INT8:
-            if res >= 128:
-                res -= 256
-            return space.newint(res)
-        elif (restype == UINT8 or
-              restype == UINT16 or
-              restype == UINT16 or
-              restype == INT16 or
-              restype == UINT32 or
-              restype == INT32):
-            return space.newint(intmask(res))
-        elif restype == INT64 or restype == UINT64:
-            bigint_res = rbigint.fromrarith_int(longlongmask(res))
-            return space.newbigint_fromrbigint(bigint_res)
-        elif restype == LONG or restype == ULONG:
-            if rffi.sizeof(rffi.LONG) < 8:
-                return space.newint(intmask(res))
-            else:
-                bigint_res = rbigint.fromrarith_int(longlongmask(res))
-                return space.newbigint_fromrbigint(bigint_res)
-        elif restype == FLOAT32 or restype == FLOAT64:
-            return space.newfloat(float(res))
-        elif restype == BOOL:
-            return space.newbool(res)
-        raise Exception("Bug in FFI: unknown Type %s" % restype)
-
-    def _ruby_wrap_STRING(self, space, res):
-        str_res = rffi.charp2str(res)
-        return space.newstr_fromstr(str_res)
-
-    def _ruby_wrap_POINTER(self, space, res):
-        int_res = rffi.cast(lltype.Signed, res)
-        w_FFI = space.find_const(space.w_kernel, 'FFI')
-        w_Pointer = space.find_const(w_FFI, 'Pointer')
-        return space.send(w_Pointer, 'new',
-                          [space.newint(int_res)])
 
     @classdef.method('attach', name='str')
     def method_attach(self, space, w_lib, name):
