922de3c4c9b0519b013fe2e7522ce9ddaef2299d
Gerd Mömken <gerd.moemken@uni-duesseldorf.de>
2013-10-01 12:06:07 +0200

    Minor change: _callback.invoke assumes ll_data is casted to VOIDP

--- tests/modules/ffi/test_callback.py
+++ tests/modules/ffi/test_callback.py
@@ -25,9 +25,11 @@ def test_invoke(space):
         p_arg2[0] = rffi.cast(rffi.INT, 7)
         p_data1[0] = rffi.cast(rffi.SIGNED, 123)
         p_data2[0] = rffi.cast(rffi.SIGNED, 468)
-        _callback.invoke(cif_descr, [p_arg1, p_arg2], p_res, p_data1)
+        _callback.invoke(cif_descr, [p_arg1, p_arg2], p_res,
+                         rffi.cast(rffi.VOIDP, p_data1))
         assert p_res[0] == 42
-        _callback.invoke(cif_descr, [p_arg1, p_arg2], p_res, p_data2)
+        _callback.invoke(cif_descr, [p_arg1, p_arg2], p_res,
+                         rffi.cast(rffi.VOIDP, p_data2))
         assert p_res[0] == 1
     finally:
         lltype.free(cif_descr, flavor='raw')
--- topaz/modules/ffi/_callback.py
+++ topaz/modules/ffi/_callback.py
@@ -1,7 +1,9 @@
+from rpython.rtyper.lltypesystem import rffi
+
 # This will definitely leak memory
 registration = {}
 
 def invoke(ll_cif, args_ll, ll_res, ll_data):
-    key = ll_data[0]
+    key = rffi.cast(rffi.SIGNEDP, ll_data)[0]
     w_proc, w_func_type = registration[key]
     w_func_type.invoke(w_proc, args_ll, ll_res)
