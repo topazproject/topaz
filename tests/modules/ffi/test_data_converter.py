963a4e16a63f9386020209ee664cfd79adb1501d
Gerd Mömken <gerd.moemken@uni-duesseldorf.de>
2013-10-14 14:30:18 +0200

    Implemented last TODO of data_converter

--- tests/modules/ffi/test_data_converter.py
+++ tests/modules/ffi/test_data_converter.py
@@ -5,8 +5,56 @@ class TestDataConverter(BaseFFITest):
         assert self.ask(space, "FFI::DataConverter.is_a? Module")
 
 class TestDataConverter__native_type(BaseFFITest):
-    def test_it_returns_nil_for_now(self, space):
-        assert self.ask(space, "FFI::DataConverter.native_type(0).nil?")
+    def test_it_raises_NotImplementedError_without_args(self, space):
+        with self.raises(space, "NotImplementedError",
+                                "native_type method not overridden and no"
+                                "native_type set"):
+            space.execute("FFI::DataConverter.native_type")
+
+    code_data_converter_test = """
+    class DataConverterTest
+      include FFI::DataConverter
+
+      def run(*args)
+        native_type(*args)
+      end
+    end
+    data_converter_test = DataConverterTest.new
+    """
+
+    def test_it_calls_find_type_if_one_arg_was_given(self, space):
+        w_res = space.execute("""
+        %s
+        def FFI.find_type(arg)
+          return arg
+        end
+        data_converter_test.run(FFI::Type::VOID)
+        """ % self.code_data_converter_test)
+        assert w_res is space.execute("FFI::Type::VOID")
+
+    def test_it_returns_the_result_of_find_type(self, ffis):
+        self.ask(ffis, """
+        %s
+        data_converter_test.run(:void).equal? FFI::Type::VOID
+        """ % self.code_data_converter_test)
+
+    def test_it_sets_the_result_of_find_type_as_attr(self, ffis):
+        w_res = ffis.execute("""
+        %s
+        data_converter_test.run(:void)
+        class DataConverterTest
+          attr_reader :native_type
+        end
+        data_converter_test.native_type
+        """ % self.code_data_converter_test)
+        assert w_res is ffis.execute("FFI::Type::VOID")
+
+    def test_it_raises_ArgumentError_for_more_than_1_arg(self, space):
+        with self.raises(space, "ArgumentError", "incorrect arguments"):
+            space.execute("""
+            %s
+            data_converter_test.run(:int8, :more)
+            """ % self.code_data_converter_test)
 
 def check_it_takes_two_args_and_returns_the_first(ffitest, space, funcname):
     w_res = space.execute("%s(1, 2)" %funcname)
--- topaz/modules/ffi/data_converter.py
+++ topaz/modules/ffi/data_converter.py
@@ -1,16 +1,21 @@
 from topaz.module import ModuleDef
+from topaz.modules.ffi.type import type_object
 
 # A rather abstract base class
 class DataConverter(object):
     moduledef = ModuleDef('DataConverter')
 
-    # TODO: If no args given raise NotIMplemented error
-    #       (see MRI ffi for error message)
-    #       If one arg is given use find_type to figure out the type
-    #       Then save the result as ruby attr (@native_type) and return it.
-    #       If more arguments were given raise ArgumentError
     @moduledef.function('native_type')
-    def native_type(self, space, args_w): pass
+    def native_type(self, space, args_w):
+        if len(args_w) == 0:
+            raise space.error(space.w_NotImplementedError,
+                              "native_type method not overridden and no"
+                              "native_type set")
+        elif len(args_w) == 1:
+            w_arg_as_type = type_object(space, args_w[0])
+            space.set_instance_var(self, '@native_type', w_arg_as_type)
+            return w_arg_as_type
+        raise space.error(space.w_ArgumentError, "incorrect arguments")
 
     @moduledef.function('to_native')
     def to_native(self, space, w_value, w_ctx):
