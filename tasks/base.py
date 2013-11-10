3128ad4674adf14fdeedd428aa416d0275dd95e4
Alex Gaynor <alex.gaynor@gmail.com>
2013-11-10 13:27:55 -0800

    Fix?

--- tasks/base.py
+++ tasks/base.py
@@ -11,3 +11,4 @@ class BaseTest(object):
     def download_rubyspec(self):
         if not os.path.isdir("../rubyspec"):
             run("cd .. && git clone --depth=100 --branch='1.x' --quiet https://github.com/rubyspec/rubyspec")
+            run("cd ../rubyspec && git checkout 1.x")
