bbb445b691f7370059c7bf9c94e2e9c6f4155273
Tim Felgentreff <timfelgentreff@gmail.com>
2016-07-06 09:25:30 +0200

    update to latest

--- tasks/base.py
+++ tasks/base.py
@@ -7,7 +7,6 @@ class BaseTest(object):
     def download_mspec(self):
         if not os.path.isdir("../mspec"):
             run("cd .. && git clone --depth=100 --quiet https://github.com/ruby/mspec")
-            run("cd ../mspec && git checkout v1.6.0")
 
     def download_rubyspec(self):
         if not os.path.isdir("../rubyspec"):
