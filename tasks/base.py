618b65f7640c071ab49a8bd9fe01f1895775e9df
Tim Felgentreff <timfelgentreff@gmail.com>
2016-07-06 09:23:22 +0200

    rubyspec and mspec moved

--- tasks/base.py
+++ tasks/base.py
@@ -6,10 +6,10 @@ from invoke import run
 class BaseTest(object):
     def download_mspec(self):
         if not os.path.isdir("../mspec"):
-            run("cd .. && git clone --depth=100 --quiet https://github.com/rubyspec/mspec")
+            run("cd .. && git clone --depth=100 --quiet https://github.com/ruby/mspec")
             run("cd ../mspec && git checkout v1.6.0")
 
     def download_rubyspec(self):
         if not os.path.isdir("../rubyspec"):
-            run("cd .. && git clone --depth=100  --quiet https://github.com/rubyspec/rubyspec")
-            run("cd ../rubyspec && git checkout v1.x")
+            run("cd .. && git clone --depth=100  --quiet https://github.com/ruby/spec")
+            run("mv spec rubyspec")
