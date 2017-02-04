ce10390ff378139d379193f2b585bc26f18f7f81
Kenichi Kamiya <kachick1@gmail.com>
2017-02-04 12:58:09 +0900

    old :<

--- tasks/base.py
+++ tasks/base.py
@@ -7,9 +7,9 @@ class BaseTest(object):
     def download_mspec(self):
         if not os.path.isdir("../mspec"):
             run("cd .. && git clone --depth=100 --quiet https://github.com/ruby/mspec")
-            run("cd ../mspec && git checkout 4bcdee51406abc739ebe2a6be81ec5a93447125e")
+            run("cd ../mspec && git checkout b363f08c6ddfbda5cf868afe9c3cc10122a0b9ab")
 
     def download_rubyspec(self):
-        if not os.path.isdir("../rubyspec"):
+        if not os.path.isdir("../spec"):
             run("cd .. && git clone --depth=100  --quiet https://github.com/ruby/spec")
-            run("cd ../spec && git checkout bdfc985bb78015614292b281cf6d2d88d4685354")
+            run("cd ../spec && git checkout 6c578cd49cb59f858d2724a687c02000e9fdaae2")
