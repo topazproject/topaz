e988aad01fdd64bd3331dbacac48000f13ddcc30
Tim Felgentreff <timfelgentreff@gmail.com>
2017-03-06 09:00:49 +0100

    try this dirty hack for osx

--- tasks/base.py
+++ tasks/base.py
@@ -1,13 +1,16 @@
 import os
 
-from invoke import run
+import invoke
+
+
+invoke.run = os.system
 
 
 class BaseTest(object):
     def download_mspec(self):
         if not os.path.isdir("../mspec"):
-            run("cd .. && git clone --depth=100 --quiet https://github.com/ruby/mspec")
+            invoke.run("cd .. && git clone --depth=100 --quiet https://github.com/ruby/mspec")
 
     def download_rubyspec(self):
         if not os.path.isdir("../rubyspec"):
-            run("cd .. && git clone --depth=100  --quiet https://github.com/ruby/spec rubyspec")
+            invoke.run("cd .. && git clone --depth=100  --quiet https://github.com/ruby/spec rubyspec")
